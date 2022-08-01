8ac32a2da4caea92531ab8eff14e12f1d9583220
Theodor Vararu <theo@vararu.org>
2022-08-01 13:05:41 +0100

    Add StaffHttpBasicAuthStrategy
    This creates an anonymous devise user which is used when there are no
    staff users defined, or when we toggle a feature flag on.
    
    Based on https://github.com/DFE-Digital/apply-for-qualified-teacher-status/pull/166

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,6 +76,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.before(:each, type: :system) { driven_by(:cuprite) }
+
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
