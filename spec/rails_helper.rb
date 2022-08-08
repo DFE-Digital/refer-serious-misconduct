19988c0b2bf48dffe955c660f36ecf8d4a8a03c0
Theodor Vararu <theo@vararu.org>
2022-08-08 17:57:45 +0100

    Add a test for inviting a staff user
    See https://github.com/DFE-Digital/apply-for-qualified-teacher-status/pull/166/commits/d77bc671fcbe38ef90a18950af3f09585009546b
    
    Co-authored-by: Malcolm Baig <git@baig.org>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -81,6 +81,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include ViewComponent::TestHelpers, type: :component
+  config.include ActiveJob::TestHelper
 end
 
 Shoulda::Matchers.configure do |config|
