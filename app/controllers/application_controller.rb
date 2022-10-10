527b04d01d51ea83ed8e4f7ee87f9db83bf80a56
Felix Clack <felixclack@gmail.com>
2022-10-10 10:37:48 +0100

    Add the boilerplate for a support interface
    We want a place to be able to administrate some of the functions of the
    service, eg. feature flags, job queues.
    
    This change introduces a support interface similar to the one we built
    for [Find](https://github.com/DFE-Digital/find-a-lost-trn/pull/53).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
+
+  http_basic_authenticate_with name: ENV.fetch("SUPPORT_USERNAME", nil),
+                               password: ENV.fetch("SUPPORT_PASSWORD", nil)
 end
