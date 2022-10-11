e50859c6d09051bff066f3456f74b8ed2350884c
Felix Clack <felixclack@gmail.com>
2022-10-11 13:08:58 +0100

    Add a service open feature flag
    We want to be able to control access to the service using a feautre
    flag.
    
    We have a new library, govuk_feature_flags, that implements the actual
    feature flag function. This change introduces that as a dependency and
    configures it for use here.

--- spec/system/support_spec.rb
+++ spec/system/support_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe "Support", type: :system do
     when_i_am_authorized_as_a_support_user
     and_i_visit_the_support_page
     then_i_see_the_support_page
+
+    when_i_visit_the_features_page
+    then_i_see_the_feature_page
   end
 
   private
@@ -20,5 +23,13 @@ RSpec.describe "Support", type: :system do
 
   def then_i_see_the_support_page
     expect(page).to have_current_path(support_interface_path)
+  end
+
+  def when_i_visit_the_features_page
+    visit support_interface_feature_flags_path
+  end
+
+  def then_i_see_the_feature_page
+    expect(page).to have_content("Features")
   end
 end
