a443077a236d6f43ff2fc5710253613365fc9cc5
Malcolm Baig <git@baig.org>
2022-10-24 16:10:54 +0100

    Add staff user invitations
    Support user invitations by doing the following:
    
    - Send Devise notifications async via ActiveJob
    - Add devise_invitable gem
    - Generate default devise_invitable config with `rails g
      devise_invitable:install`
    - Set invite period to 1 week
    - Add all required database fields

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -45,6 +45,7 @@ class Staff < ApplicationRecord
   devise(
     :confirmable,
     :database_authenticatable,
+    :invitable,
     :lockable,
     :recoverable,
     :registerable,
@@ -53,4 +54,8 @@ class Staff < ApplicationRecord
     :trackable,
     :validatable,
   )
+
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
