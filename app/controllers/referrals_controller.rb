c9552c617522e71a0537bcc457ae61264dc5cdc4
Steve Laing <steve.laing@gmail.com>
2022-10-25 17:14:45 +0100

    Protect referrals actions with feature flag

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -1,4 +1,6 @@
 class ReferralsController < ApplicationController
+  before_action :check_employer_form_feature_flag_enabled
+
   def new
     @referral_form = ReferralForm.new(referral: Referral.new)
   end
@@ -27,5 +29,11 @@ class ReferralsController < ApplicationController
   def referral
     # TODO: This needs integration with current_user check
     @referral ||= Referral.find(params[:id])
+  end
+
+  def check_employer_form_feature_flag_enabled
+    unless FeatureFlags::FeatureFlag.active?(:employer_form)
+      redirect_to start_path
+    end
   end
 end
