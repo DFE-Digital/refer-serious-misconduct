3851ce7b661ab75e116b09927fdf29ed93446de0
Steve Laing <steve.laing@gmail.com>
2022-10-25 16:46:56 +0100

    Add Referral model

---
+++ app/models/referral.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: referrals
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Referral < ApplicationRecord
+end
