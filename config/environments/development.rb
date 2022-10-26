26b7e23e1db872ae719266e4acc0bd884ab5b81f
Malcolm Baig <git@baig.org>
2022-10-26 16:16:40 +0100

    Add sent emails to log/mail.log
    It's useful to inspect the contents of sent email during development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -60,7 +60,20 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :notify
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.logger =
+    Logger.new(
+      "log/mail.log",
+      formatter:
+        proc do |_, _, _, msg|
+          if msg =~ /quoted-printable/
+            message = Mail::Message.new(msg)
+            "\nTo: #{message.to}\n\n#{message.decoded}\n\n"
+          else
+            "\n#{msg}"
+          end
+        end,
+    )
 end
