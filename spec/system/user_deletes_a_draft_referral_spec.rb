1d817ab65b0de9a3eae31ea5016b64b365451a3c
Steve Laing <steve.laing@gmail.com>
2022-10-26 15:49:17 +0100

    Integrate new/edit referral flows with confirming deletion

--- spec/system/user_deletes_a_draft_referral_spec.rb
+++ spec/system/user_deletes_a_draft_referral_spec.rb
@@ -5,9 +5,16 @@ RSpec.feature "User deletes a draft referral" do
   scenario "User deletes a draft referral" do
     given_the_service_is_open
     and_the_employer_form_feature_is_active
-    and_i_have_an_existing_referral
-    when_i_visit_the_referral_summary
+
+    when_i_make_a_new_referral
     and_i_dont_want_to_continue
+    and_i_confirm_deletion
+    then_the_new_referral_is_discarded
+
+    when_i_have_an_existing_referral
+    and_i_visit_the_referral_summary
+    and_i_dont_want_to_continue
+    and_i_confirm_deletion
     then_the_draft_referral_is_deleted
   end
 
@@ -21,21 +28,36 @@ RSpec.feature "User deletes a draft referral" do
     FeatureFlags::FeatureFlag.activate(:employer_form)
   end
 
-  def and_i_have_an_existing_referral
+  def when_i_have_an_existing_referral
     @referral = Referral.create!
   end
 
-  def when_i_visit_the_referral_summary
+  def and_i_visit_the_referral_summary
     visit edit_referral_path(@referral)
   end
 
+  def when_i_make_a_new_referral
+    visit new_referral_path
+  end
+
   def and_i_dont_want_to_continue
-    find(:button, text: "Delete your draft referral", visible: false).trigger(
-      "click"
-    )
+    find(
+      "a.govuk-button",
+      text: "Delete your draft referral",
+      visible: false
+    ).trigger("click")
+  end
+
+  def and_i_confirm_deletion
+    click_on "Yes I’m sure – delete it"
   end
 
   def then_the_draft_referral_is_deleted
     expect { @referral.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect(page).to have_content("Your draft has been deleted")
+  end
+
+  def then_the_new_referral_is_discarded
+    expect(page).to have_content("Your draft has been deleted")
   end
 end
