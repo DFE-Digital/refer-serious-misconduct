DFE-Digital/find-a-lost-trn

Send staff to support interface after being invited
This takes them to a place which is relevant once they've been invited
and signed in.

See https://github.com/DFE-Digital/apply-for-qualified-teacher-status/pull/166/commits/e19d72fd8efeb71dab732068115de1cd353da086

app/controllers/staff/invitations_controller.rb
@@ -35,4 +35,8 @@ class Staff::InvitationsController < Devise::InvitationsController
       super
     end
   end
+
+  def after_accept_path_for(resource)
+    resource.is_a?(Staff) ? support_interface_path : super
+  end
 end

DFE-Digital/refer-serious-misconduct

app/controllers/staff/invitations_controller.rb
@@ -37,4 +37,8 @@ class Staff::InvitationsController < Devise::InvitationsController
       super
     end
   end
+
+  def after_accept_path_for(resource)
+    resource.is_a?(Staff) ? support_interface_root_path : super
+  end
 end