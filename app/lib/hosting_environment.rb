e1a61fc1fe38e4cb5a7d245bada7027505923e03
Malcolm Baig <git@baig.org>
2022-10-26 12:17:48 +0100

    Add HostingEnvironment class
    Assists with inspecting the app runtime environment in business logic
    or configuration.

---
+++ app/lib/hosting_environment.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+module HostingEnvironment
+  TEST_ENVIRONMENTS = %w[local test development review].freeze
+  PRODUCTION_URL = I18n.t("service.url")
+
+  def self.host
+    ENV.fetch("HOSTING_DOMAIN")
+  end
+
+  def self.environment_name
+    ENV.fetch("HOSTING_ENVIRONMENT_NAME", "unknown-environment")
+  end
+
+  def self.test_environment?
+    TEST_ENVIRONMENTS.include?(HostingEnvironment.environment_name)
+  end
+
+  def self.production?
+    environment_name == "production"
+  end
+end
