7cbcf805bec89175147a59d415f1d6f4f3e5e880
Steve Laing <steve.laing@gmail.com>
2022-10-27 15:49:52 +0100

    Add name fields to referrals

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -2,9 +2,14 @@
 #
 # Table name: referrals
 #
-#  id         :bigint           not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id               :bigint           not null, primary key
+#  first_name       :string
+#  last_name        :string
+#  name_has_changed :integer
+#  previous_name    :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 class Referral < ApplicationRecord
+  enum :name_has_changed, { yes: 0, no: 1, dont_know: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_103526) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_155534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_103526) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_feature_flags_features_on_name", unique: true
+  end
+
+  create_table "referrals", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "previous_name"
+    t.integer "name_has_changed"
   end
 
   create_table "staff", force: :cascade do |t|
@@ -70,8 +79,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_103526) do
     t.index ["unlock_token"], name: "index_staff_on_unlock_token", unique: true
   end
 
-  create_table "referrals", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
