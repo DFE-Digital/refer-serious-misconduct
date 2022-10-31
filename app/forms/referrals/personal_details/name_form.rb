d5182fa528a9e0537fa142d16ba7a20d7dcdcb24
Steve Laing <steve.laing@gmail.com>
2022-10-31 15:02:48 +0000

    Add form object for personal details name fields

---
+++ app/forms/referrals/personal_details/name_form.rb
@@ -0,0 +1,29 @@
+module Referrals
+  module PersonalDetails
+    class NameForm
+      include ActiveModel::Model
+
+      attr_accessor :first_name,
+                    :last_name,
+                    :name_has_changed,
+                    :previous_name,
+                    :referral
+
+      validates :first_name, :last_name, presence: true
+      validates :previous_name,
+                presence: true,
+                if: -> { name_has_changed == "yes" }
+
+      def save
+        if valid?
+          referral.update!(
+            first_name:,
+            last_name:,
+            name_has_changed:,
+            previous_name:
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/forms/referrals/personal_details/name_form_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe Referrals::PersonalDetails::NameForm do
+  describe "#save (names)" do
+    let(:referral) { Referral.new }
+    let(:first_name) { "Jane" }
+    let(:last_name) { "Smith" }
+    let(:name_has_changed) { "yes" }
+    let(:previous_name) { "Janet Jones" }
+
+    subject(:form) do
+      described_class.new(
+        referral:,
+        first_name:,
+        last_name:,
+        name_has_changed:,
+        previous_name:
+      )
+    end
+
+    context "with valid values" do
+      it "updates the names on the referral record" do
+        form.save
+
+        expect(referral.first_name).to eq("Jane")
+        expect(referral.last_name).to eq("Smith")
+        expect(referral.name_has_changed).to eq("yes")
+        expect(referral.previous_name).to eq("Janet Jones")
+      end
+    end
+
+    context "with invalid values" do
+      let(:first_name) { "" }
+      let(:last_name) { "" }
+      let(:name_has_changed) { "yes" }
+      let(:previous_name) { "" }
+
+      it "fails form validation" do
+        form.save
+
+        expect(form.errors[:first_name]).to include "First name can't be blank"
+        expect(form.errors[:last_name]).to include "Last name can't be blank"
+        expect(
+          form.errors[:previous_name]
+        ).to include "Previous name can't be blank"
+      end
+    end
+  end
+end
