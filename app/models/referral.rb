a380ff92fe83bb8563f68fbc1e3d44ac5e6eabd6
Steve Laing <steve.laing@gmail.com>
2022-11-01 13:20:16 +0000

    Remove enum from Referral
    We'll handle input with form validations

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -3,13 +3,15 @@
 # Table name: referrals
 #
 #  id               :bigint           not null, primary key
+#  age_known        :integer
+#  approximate_age  :string
+#  date_of_birth    :date
 #  first_name       :string
 #  last_name        :string
-#  name_has_changed :integer
+#  name_has_changed :string
 #  previous_name    :string
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #
 class Referral < ApplicationRecord
-  enum :name_has_changed, { yes: 0, no: 1, dont_know: 2 }
 end
