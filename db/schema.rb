6bbbfec9a198a680c86da462015e9b218adc9463
Malcolm Baig <git@baig.org>
2022-11-02 15:25:06 +0000

    Add User model
    Has the minimum necessary fields for:
    
    - An OTP-based authentication flow
    - Tracking user sign ins

--- db/schema.rb
+++ db/schema.rb
@@ -104,5 +104,19 @@ ActiveRecord::Schema[7.0].define(version: 20_221_104_124_508) do
     t.index ["unlock_token"], name: "index_staff_on_unlock_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "secret_key"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
   add_foreign_key "referrers", "referrals"
 end
