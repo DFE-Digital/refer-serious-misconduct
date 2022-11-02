387833f0b8b2ef4d7e82ca4173cb93ccafc3f8a5
Malcolm Baig <git@baig.org>
2022-11-02 15:26:39 +0000

    Add controllers, views and strategy for OTP authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,18 @@
 #
 class User < ApplicationRecord
   devise :validatable, :trackable
+
+  attr_reader :otp
+
+  def password_required?
+    false
+  end
+
+  def password
+    nil
+  end
+
+  def after_otp_authentication
+    update(secret_key: nil)
+  end
 end
