6f6d434acb56807f7f98f734a8fa89c00ad17fa0
Malcolm Baig <git@baig.org>
2022-11-02 15:25:06 +0000

    Add User model
    Has the minimum necessary fields for:
    
    - An OTP-based authentication flow
    - Tracking user sign ins

---
+++ spec/factories/users.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :bigint           not null, primary key
+#  current_sign_in_at  :datetime
+#  current_sign_in_ip  :string
+#  email               :string           default(""), not null
+#  last_sign_in_at     :datetime
+#  last_sign_in_ip     :string
+#  remember_created_at :datetime
+#  secret_key          :string
+#  sign_in_count       :integer          default(0), not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+FactoryBot.define do
+  factory :user do
+    
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :bigint           not null, primary key
+#  current_sign_in_at  :datetime
+#  current_sign_in_ip  :string
+#  email               :string           default(""), not null
+#  last_sign_in_at     :datetime
+#  last_sign_in_ip     :string
+#  remember_created_at :datetime
+#  secret_key          :string
+#  sign_in_count       :integer          default(0), not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+end
