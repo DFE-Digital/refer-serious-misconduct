45018de50970afac84bf023d22a2732f59d70c53
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-11-03 11:15:03 +0000

    Add address fields to referrals table

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -3,15 +3,21 @@
 # Table name: referrals
 #
 #  id               :bigint           not null, primary key
+#  address_known    :boolean
+#  address_line_1   :string
+#  address_line_2   :string
 #  age_known        :string
 #  approximate_age  :string
+#  country          :string
 #  date_of_birth    :date
 #  email_address    :string(256)
 #  email_known      :boolean
 #  first_name       :string
 #  last_name        :string
 #  name_has_changed :string
+#  postcode         :string(11)
 #  previous_name    :string
+#  town_or_city     :string
 #  trn              :string
 #  trn_known        :boolean
 #  created_at       :datetime         not null
