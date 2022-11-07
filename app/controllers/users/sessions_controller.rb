0786da84407ae830d29d1108456cccc77d1d4efa
Malcolm Baig <git@baig.org>
2022-11-07 14:39:13 +0000

    Remove comments in Users::SessionsController

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,16 +3,9 @@ class Users::SessionsController < Devise::SessionsController
     self.resource = resource_class.find_or_initialize_by(sign_in_params)
 
     if resource.save
-      # generate secret and otp
       secret_key = ROTP::Base32.random
-      # otp = ROTP::HOTP.new(secret_key).at(0)
-
-      # save 32-bit key to model
       resource.update(secret_key:)
-
       # TODO: send otp via email
-
-      # redirect to OTP controller
       redirect_to new_user_otp_path(id: resource.id)
     else
       render :new
