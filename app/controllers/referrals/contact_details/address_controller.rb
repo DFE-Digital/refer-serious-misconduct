ee2338746af519e391108cd723605d9534c082bb
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-11-08 15:45:57 +0000

    Use current_referral in contact details controllers

--- app/controllers/referrals/contact_details/address_controller.rb
+++ app/controllers/referrals/contact_details/address_controller.rb
@@ -1,24 +1,29 @@
 module Referrals
   module ContactDetails
-    class AddressController < ReferralsController
+    class AddressController < Referrals::BaseController
       def edit
         @contact_details_address_form =
           AddressForm.new(
-            address_known: referral.address_known,
-            address_line_1: referral.address_line_1,
-            address_line_2: referral.address_line_2,
-            town_or_city: referral.town_or_city,
-            postcode: referral.postcode,
-            country: referral.country
+            address_known: current_referral.address_known,
+            address_line_1: current_referral.address_line_1,
+            address_line_2: current_referral.address_line_2,
+            town_or_city: current_referral.town_or_city,
+            postcode: current_referral.postcode,
+            country: current_referral.country
           )
       end
 
       def update
         @contact_details_address_form =
-          AddressForm.new(contact_details_address_form_params.merge(referral:))
+          AddressForm.new(
+            contact_details_address_form_params.merge(
+              referral: current_referral
+            )
+          )
         if @contact_details_address_form.save
-          # TODO: Redirect to check answers page
-          redirect_to edit_referral_path(referral)
+          redirect_to referrals_update_contact_details_check_answers_path(
+                        current_referral
+                      )
         else
           render :edit
         end
--- app/controllers/referrals/contact_details/email_controller.rb
+++ app/controllers/referrals/contact_details/email_controller.rb
@@ -1,19 +1,23 @@
 module Referrals
   module ContactDetails
-    class EmailController < ReferralsController
+    class EmailController < Referrals::BaseController
       def edit
         @contact_details_email_form =
           EmailForm.new(
-            email_known: referral.email_known,
-            email_address: referral.email_address
+            email_known: current_referral.email_known,
+            email_address: current_referral.email_address
           )
       end
 
       def update
         @contact_details_email_form =
-          EmailForm.new(contact_details_email_form_params.merge(referral:))
+          EmailForm.new(
+            contact_details_email_form_params.merge(referral: current_referral)
+          )
         if @contact_details_email_form.save
-          redirect_to referrals_update_contact_details_telephone_path(referral)
+          redirect_to referrals_update_contact_details_telephone_path(
+                        current_referral
+                      )
         else
           render :edit
         end
