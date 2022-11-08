1ef5a4572bd4fd6b15afcf5d4849b3ccb7a953ae
Steve Laing <steve.laing@gmail.com>
2022-11-08 18:04:35 +0000

    Make NameController inherit from BaseController

--- app/controllers/referrals/personal_details/name_controller.rb
+++ app/controllers/referrals/personal_details/name_controller.rb
@@ -1,22 +1,23 @@
 module Referrals
   module PersonalDetails
-    class NameController < ReferralsController
+    class NameController < Referrals::BaseController
       def edit
         @personal_details_name_form =
           NameForm.new(
-            referral:,
-            first_name: referral.first_name,
-            last_name: referral.last_name,
-            name_has_changed: referral.name_has_changed,
-            previous_name: referral.previous_name
+            referral: current_referral,
+            first_name: current_referral.first_name,
+            last_name: current_referral.last_name,
+            name_has_changed: current_referral.name_has_changed,
+            previous_name: current_referral.previous_name
           )
       end
 
       def update
-        @personal_details_name_form = NameForm.new(name_params.merge(referral:))
+        @personal_details_name_form =
+          NameForm.new(name_params.merge(referral: current_referral))
 
         if @personal_details_name_form.save
-          redirect_to referrals_edit_personal_details_age_path(referral)
+          redirect_to referrals_edit_personal_details_age_path(current_referral)
         else
           render :edit
         end
