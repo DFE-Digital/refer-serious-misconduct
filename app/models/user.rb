a52830d409d03c0d3452fd4c9dbd58756792fc14
Malcolm Baig <git@baig.org>
2022-11-09 12:55:22 +0000

    Conditionally redirect signed in user
    If the user has existing referrals, redirect them to the show page for
    the most recently created one. If not, take them to the start page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
   include Devise::Models::OtpAuthenticatable
 
   has_many :referrals
+
+  def latest_referral
+    referrals.order(created_at: :desc).first
+  end
 end
