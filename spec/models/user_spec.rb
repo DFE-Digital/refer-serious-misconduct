abbc1e840aed38e03eefb22b4ab05b5220193e5d
Malcolm Baig <git@baig.org>
2022-11-09 12:55:22 +0000

    Conditionally redirect signed in user
    If the user has existing referrals, redirect them to the show page for
    the most recently created one. If not, take them to the start page.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,4 +21,14 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  describe "#latest_referral" do
+    it "returns the most recently created referral" do
+      user = create(:user)
+      expected_referral = create(:referral, user:, created_at: Time.zone.now + 1.hour)
+      create(:referral, user:, created_at: Time.zone.now)
+      create(:referral, user:, created_at: Time.zone.now - 1.hour)
+
+      expect(user.latest_referral).to eq expected_referral
+    end
+  end
 end
