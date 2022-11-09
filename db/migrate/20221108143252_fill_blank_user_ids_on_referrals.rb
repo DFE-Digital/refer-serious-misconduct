e77f86ba412df079f173d95b512a447cecc42941
Malcolm Baig <git@baig.org>
2022-11-09 13:20:03 +0000

    Lint user/referral association work

--- db/migrate/20221108143252_fill_blank_user_ids_on_referrals.rb
+++ db/migrate/20221108143252_fill_blank_user_ids_on_referrals.rb
@@ -2,8 +2,6 @@ class FillBlankUserIdsOnReferrals < ActiveRecord::Migration[7.0]
   def change
     # Fill in null user_ids
     # This will be followed by a migration setting this column to not null.
-    if Referral.exists?
-      Referral.update_all(user_id: 1)
-    end
+    Referral.update_all(user_id: 1) if Referral.exists?
   end
 end
