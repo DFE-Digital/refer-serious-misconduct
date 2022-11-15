a03ab00f47d0e3b5c00448be18ad8ab7731c59fe
NickGraham101 <nick.graham101@googlemail.com>
2022-11-15 14:05:19 +0000

    Updated workflow & action to deploy review app
    Override default site up retries for review app

--- terraform/review-app.tf
+++ terraform/review-app.tf
@@ -1,5 +1,6 @@
 resource "azurerm_resource_group" "review_app_rg" {
   count    = var.environment_name == "review" ? 1 : 0
   name     = var.resource_group_name
-  location = "West Europe"
+  location = var.region_name
+  tags     = jsondecode(var.resource_group_tags)
 }
