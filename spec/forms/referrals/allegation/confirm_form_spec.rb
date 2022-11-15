be27a8db18bd0208232b0a75e9f3889e850f06ba
Steve Laing <steve.laing@gmail.com>
2022-11-15 18:00:48 +0000

    Add form object to confirm allegation section

---
+++ spec/forms/referrals/allegation/confirm_form_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe Referrals::Allegation::ConfirmForm, type: :model do
+  describe "#save" do
+    let(:referral) { build(:referral) }
+    let(:allegation_details_complete) { "false" }
+    subject(:save) { confirm_form.save }
+
+    let(:confirm_form) do
+      described_class.new(referral:, allegation_details_complete:)
+    end
+
+    context "with a valid value" do
+      it "saves the value on the referral" do
+        save
+        expect(referral.allegation_details_complete).to eq(false)
+      end
+    end
+
+    context "with no values" do
+      let(:allegation_details_complete) { nil }
+      it "adds an error" do
+        save
+        expect(confirm_form.errors[:allegation_details_complete]).to eq(
+          ["Tell us if you have completed this section"]
+        )
+      end
+    end
+  end
+end
