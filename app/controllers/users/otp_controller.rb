a6323da4c65817653c4564250a80af00ee47131e
Malcolm Baig <git@baig.org>
2022-11-15 11:29:17 +0000

    Change sign in flash banner to :success

--- app/controllers/users/otp_controller.rb
+++ app/controllers/users/otp_controller.rb
@@ -19,7 +19,7 @@ class Users::OtpController < DeviseController
 
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
+    set_flash_message!(:success, :signed_in)
     sign_in(resource_name, resource)
     yield resource if block_given?
     redirect_to after_sign_in_path_for(resource)
