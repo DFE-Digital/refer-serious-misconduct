f5b2e6642f2728caebdefe01cbeebc7f88087a9b
Steve Laing <steve.laing@gmail.com>
2022-11-15 16:00:48 +0000

    Add actions and views for allegation details

---
+++ app/controllers/referrals/allegation/details_controller.rb
@@ -0,0 +1,32 @@
+module Referrals
+  module Allegation
+    class DetailsController < Referrals::BaseController
+      def edit
+        @allegation_details_form = DetailsForm.new(referral: current_referral)
+      end
+
+      def update
+        @allegation_details_form =
+          DetailsForm.new(
+            allegation_details_params.merge(referral: current_referral)
+          )
+
+        if @allegation_details_form.save
+          # Redirect to next step
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def allegation_details_params
+        params.require(:referrals_allegation_details_form).permit(
+          :allegation_format,
+          :allegation_details,
+          :allegation_upload
+        )
+      end
+    end
+  end
+end
