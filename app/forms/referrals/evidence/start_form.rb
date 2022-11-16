0bbb2daa74f92d557452ba03bbbb6a625851a200
Steve Laing <steve.laing@gmail.com>
2022-11-16 14:46:43 +0000

    Add form object for first step of evidence flow

---
+++ app/forms/referrals/evidence/start_form.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+module Referrals
+  module Evidence
+    class StartForm
+      include ActiveModel::Model
+
+      attr_accessor :referral
+      attr_reader :has_evidence
+
+      validates :has_evidence, inclusion: { in: [true, false] }
+
+      def has_evidence=(value)
+        @has_evidence = ActiveModel::Type::Boolean.new.cast(value)
+      end
+
+      def save
+        return false if invalid?
+
+        referral.update(has_evidence:)
+        true
+      end
+    end
+  end
+end
