ad63e157db0a0a22c1ec147d873d81592acf86f1
Steve Laing <steve.laing@gmail.com>
2022-11-16 14:46:43 +0000

    Add form object for first step of evidence flow

---
+++ spec/forms/referrals/evidence/start_form_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe Referrals::Evidence::StartForm, type: :model do
+  describe "#save" do
+    let(:referral) { build(:referral) }
+    let(:has_evidence) { "false" }
+    subject(:save) { start_form.save }
+
+    let(:start_form) { described_class.new(referral:, has_evidence:) }
+
+    context "with a valid value" do
+      it "saves the value on the referral" do
+        save
+        expect(referral.has_evidence).to eq(false)
+      end
+    end
+
+    context "with no values" do
+      let(:has_evidence) { nil }
+      it "adds an error" do
+        save
+        expect(start_form.errors[:has_evidence]).to eq(
+          ["Tell us if you have evidence to upload"]
+        )
+      end
+    end
+  end
+end
