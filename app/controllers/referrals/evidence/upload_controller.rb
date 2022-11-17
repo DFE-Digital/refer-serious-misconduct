3e970dee62bb2cd5a272ffbba86acdf63118cfb2
Steve Laing <steve.laing@gmail.com>
2022-11-17 11:36:53 +0000

    Add routes, actions, views for uploading evidence

---
+++ app/controllers/referrals/evidence/upload_controller.rb
@@ -0,0 +1,31 @@
+module Referrals
+  module Evidence
+    class UploadController < Referrals::BaseController
+      def edit
+        @evidence_upload_form = UploadForm.new(referral: current_referral)
+      end
+
+      def show
+      end
+
+      def update
+        @evidence_upload_form =
+          UploadForm.new(upload_params.merge(referral: current_referral))
+
+        if @evidence_upload_form.save
+          redirect_to referrals_evidence_uploaded_path(current_referral)
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def upload_params
+        params.require(:referrals_evidence_upload_form).permit(
+          evidence_uploads: []
+        )
+      end
+    end
+  end
+end
