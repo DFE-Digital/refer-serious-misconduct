d1b452643200579650ddf755bdd290bae4707c9f
NickGraham101 <nick.graham101@googlemail.com>
2022-11-17 14:51:05 +0000

    Dynamically assign terraform outputs to GitHub env vars
    Removes the need to hardcode terraform outputs into the action reducing
    duplication.  Smoke test action uses an input instead of referencing a
    GITHUB_ENV var set in another action.

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,12 +1,21 @@
 output "app_fqdn" {
-  value = var.domain != null ? var.domain : azurerm_linux_web_app.rsm-app.default_hostname
+  value = "https://${var.domain != null ? var.domain : azurerm_linux_web_app.rsm-app.default_hostname}"
 }
+
+output "app_resource_group_name" {
+  value = data.azurerm_resource_group.group.name
+}
+
 output "postgres_server_name" {
   value = azurerm_postgresql_flexible_server.postgres-server.name
 }
 
 output "blue_green" {
   value = var.enable_blue_green
+}
+
+output "web_app_name" {
+  value = local.rsm_app_name
 }
 
 output "web_app_slot_name" {
