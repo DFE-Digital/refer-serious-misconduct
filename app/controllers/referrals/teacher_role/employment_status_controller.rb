01def7bbf8bcc21d563fc1964e31e561e9f229b3
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2022-11-21 11:04:19 +0000

    Add employment status page
    The employment status page follows the start date page in the "About
    their role" journey.

---
+++ app/controllers/referrals/teacher_role/employment_status_controller.rb
@@ -0,0 +1,46 @@
+module Referrals
+  module TeacherRole
+    class EmploymentStatusController < Referrals::BaseController
+      def edit
+        @employment_status_form =
+          EmploymentStatusForm.new(
+            referral: current_referral,
+            employment_status: current_referral.employment_status,
+            role_end_date: current_referral.role_end_date,
+            reason_leaving_role: current_referral.reason_leaving_role
+          )
+      end
+
+      def update
+        @employment_status_form =
+          EmploymentStatusForm.new(
+            employment_status_params.merge(referral: current_referral)
+          )
+
+        if @employment_status_form.save(end_date_params)
+          # TODO: redirect to job title page
+          redirect_to edit_referral_path(current_referral)
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def employment_status_params
+        params.require(:referrals_teacher_role_employment_status_form).permit(
+          :employment_status,
+          :reason_leaving_role
+        )
+      end
+
+      def end_date_params
+        params.require(:referrals_teacher_role_employment_status_form).permit(
+          "role_end_date(3i)",
+          "role_end_date(2i)",
+          "role_end_date(1i)"
+        )
+      end
+    end
+  end
+end
