b6e4666d3166b65097ff96d4c3d7928bd2c87154
Steve Laing <steve.laing@gmail.com>
2022-11-21 15:54:00 +0000

    Add routes and action for evidence upload categories

---
+++ app/controllers/referrals/evidence/categories_controller.rb
@@ -0,0 +1,48 @@
+module Referrals
+  module Evidence
+    class CategoriesController < Referrals::BaseController
+      def edit
+        @evidence_categories_form =
+          CategoriesForm.new(referral: current_referral, evidence:)
+      end
+
+      def show
+      end
+
+      def update
+        @evidence_categories_form =
+          CategoriesForm.new(
+            categories_params.merge(referral: current_referral, evidence:)
+          )
+
+        if @evidence_categories_form.save
+          next_evidence = @evidence_categories_form.next_evidence
+          if next_evidence.present?
+            redirect_to referrals_update_evidence_categories_path(
+                          current_referral,
+                          next_evidence
+                        )
+          else
+            redirect_to referrals_edit_evidence_confirm_path(current_referral)
+          end
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def categories_params
+        params.require(:referrals_evidence_categories_form).permit(
+          :categories_other,
+          categories: []
+        )
+      end
+
+      def evidence
+        @evidence ||= current_referral.evidences.find(params[:evidence_id])
+      end
+      helper_method :evidence
+    end
+  end
+end
