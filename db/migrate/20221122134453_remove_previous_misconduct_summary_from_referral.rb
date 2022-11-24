9d89a609ec6056891a33a96ee0bd61f978e3aaf7
Malcolm Baig <git@baig.org>
2022-11-24 10:59:56 +0000

    Amend referrals 'previous_misconduct_summary' column removal
    This is causing errors in the deployed environments because the column
    is already absent for some reason but the migration is being run. Try
    adding `if_exists` to get the migrations passing.

--- db/migrate/20221122134453_remove_previous_misconduct_summary_from_referral.rb
+++ db/migrate/20221122134453_remove_previous_misconduct_summary_from_referral.rb
@@ -1,5 +1,8 @@
 class RemovePreviousMisconductSummaryFromReferral < ActiveRecord::Migration[7.0]
   def change
-    remove_column :referrals, :previous_misconduct_summary, :text
+    remove_column :referrals,
+                  :previous_misconduct_summary,
+                  :text,
+                  if_exists: true
   end
 end
