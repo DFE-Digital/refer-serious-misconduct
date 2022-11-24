ab894f5557acab43a853ab635c055cd20f57f64a
Steve Laing <steve.laing@gmail.com>
2022-11-24 16:50:35 +0000

    Add file upload validator

---
+++ app/validators/file_upload_validator.rb
@@ -0,0 +1,45 @@
+class FileUploadValidator < ActiveModel::EachValidator
+  MAX_FILE_SIZE = 25.megabytes
+
+  CONTENT_TYPES = {
+    ".pdf" => "application/pdf",
+    ".doc" => "application/msword",
+    ".docx" =>
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    ".txt" => "text/plain"
+  }.freeze
+
+  def validate_each(record, attribute, uploaded_files)
+    Array(uploaded_files).compact_blank.each do |uploaded_file|
+      next if uploaded_file.nil?
+      next if uploaded_file.size.nil?
+
+      if uploaded_file.size >= MAX_FILE_SIZE
+        record.errors.add attribute, :file_size_too_big
+        break
+      end
+
+      content_type =
+        Marcel::MimeType.for(
+          uploaded_file,
+          name: uploaded_file.original_filename,
+          declared_type: uploaded_file.content_type
+        )
+
+      extension = File.extname(uploaded_file.original_filename).downcase
+
+      if !CONTENT_TYPES.values.include?(content_type) ||
+           !CONTENT_TYPES.keys.include?(extension)
+        record.errors.add(
+          attribute,
+          :invalid_content_type,
+          valid_types: CONTENT_TYPES.keys.sort.join(", ")
+        )
+        break
+      elsif CONTENT_TYPES[extension] != content_type
+        record.errors.add attribute, :mismatch_content_type
+        break
+      end
+    end
+  end
+end
