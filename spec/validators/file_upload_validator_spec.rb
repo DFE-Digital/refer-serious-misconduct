2a11c399e4b007b4b040024047337a975fae5cb2
Steve Laing <steve.laing@gmail.com>
2022-11-24 16:50:35 +0000

    Add file upload validator

---
+++ spec/validators/file_upload_validator_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe FileUploadValidator do
+  before do
+    stub_const("Validatable", Class.new).class_eval do
+      include ActiveModel::Validations
+      attr_accessor :file
+      validates :file, file_upload: true
+    end
+  end
+
+  subject(:model) { Validatable.new }
+
+  before { model.file = file }
+
+  context "with a valid file" do
+    let(:file) { fixture_file_upload("upload.pdf", "application/pdf") }
+
+    it { is_expected.to be_valid }
+  end
+
+  context "with a valid file with an uppercase extension" do
+    let(:file) { fixture_file_upload("upload.PDF", "application/pdf") }
+
+    it { is_expected.to be_valid }
+  end
+
+  context "with an invalid content type" do
+    let(:file) { fixture_file_upload("upload.pl", "application/x-perl") }
+
+    it { is_expected.to_not be_valid }
+  end
+
+  context "with an invalid extension" do
+    let(:file) { fixture_file_upload("upload.txt", "application/pdf") }
+
+    it { is_expected.to_not be_valid }
+  end
+
+  context "with a large file" do
+    let(:file) { fixture_file_upload("upload.pdf", "application/pdf") }
+
+    before { allow(file).to receive(:size).and_return(25 * 1024 * 1024) }
+
+    it { is_expected.to_not be_valid }
+  end
+end
