60166d64d394d673a8ce37b45e3220e0b13e51a9
Malcolm Baig <git@baig.org>
2022-11-29 16:32:41 +0000

    Add test users to support interface
    Introduce functionality that allows support staff to sign in with a test user
    account.
    
    The Test Users section of the support interface lists user accounts that
    have been created on the environment. Support staff can either sign in
    as an existing user or create a new one.
    
    This functionality is restricted to test environments.

---
+++ app/controllers/support_interface/test_users_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+module SupportInterface
+  class TestUsersController < SupportInterfaceController
+    before_action :redirect_unless_test_environment
+
+    def index
+      @users = User.newest_first
+    end
+
+    def create
+      user = FactoryBot.create(:user)
+      sign_in(user)
+      redirect_to root_path
+    end
+
+    def authenticate
+      user = User.find(params.fetch(:id))
+      sign_in(user)
+      redirect_to root_path
+    end
+
+    private
+
+    def redirect_unless_test_environment
+      unless HostingEnvironment.test_environment?
+        flash[:warning] = "Test users can only be accessed on test environments"
+        redirect_to support_interface_root_path
+      end
+    end
+  end
+end
