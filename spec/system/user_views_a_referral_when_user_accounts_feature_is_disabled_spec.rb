41a1ad115f05e5d290d33e58d2e009209c613005
Steve Laing <steve.laing@gmail.com>
2022-11-29 10:02:09 +0000

    Redirect away from referrals when user accounts feature is inactive

---
+++ spec/system/user_views_a_referral_when_user_accounts_feature_is_disabled_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.feature "User accounts disabled, user views a referral summary",
+              type: :system do
+  include CommonSteps
+
+  scenario "User views referral summary" do
+    given_the_service_is_open
+    and_the_employer_form_feature_is_active
+    and_the_eligbility_screener_feature_is_active
+    and_the_user_accounts_feature_is_inactive
+    and_there_is_an_existing_referral
+    when_i_visit_the_referral_summary
+    then_i_am_redirected_to_the_start_page
+  end
+
+  private
+
+  def and_there_is_an_existing_referral
+    @referral = create(:referral, user: create(:user))
+  end
+
+  def and_the_employer_form_feature_is_active
+    FeatureFlags::FeatureFlag.activate(:employer_form)
+  end
+
+  def and_the_eligbility_screener_feature_is_active
+    FeatureFlags::FeatureFlag.activate(:eligibility_screener)
+  end
+
+  def given_the_service_is_open
+    FeatureFlags::FeatureFlag.activate(:service_open)
+  end
+
+  def and_the_user_accounts_feature_is_inactive
+    FeatureFlags::FeatureFlag.deactivate(:user_accounts)
+  end
+
+  def when_i_visit_the_referral_summary
+    visit edit_referral_path(@referral)
+  end
+
+  def then_i_am_redirected_to_the_start_page
+    expect(current_path).to eq(start_path)
+  end
+end
