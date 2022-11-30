87294fac9cb4c95d90450437e73d36be3acecf68
Malcolm Baig <git@baig.org>
2022-11-30 11:29:22 +0000

    Add system spec for Test Users functionality

---
+++ spec/system/support/staff_user_signs_in_as_user_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.feature "Test users" do
+  include CommonSteps
+
+  scenario "Staff user signs in as user" do
+    given_the_service_is_open
+    and_the_employer_form_feature_is_active
+    and_the_eligbility_screener_feature_is_active
+    and_the_user_accounts_feature_is_active
+    and_staff_http_basic_is_active
+    when_i_am_authorized_as_a_support_user
+
+    and_i_visit_the_test_users_section
+    and_i_create_a_new_user
+    then_i_am_signed_in_as_that_user
+  end
+
+  private
+
+  def and_staff_http_basic_is_active
+    FeatureFlags::FeatureFlag.activate(:staff_http_basic_auth)
+  end
+
+  def when_i_am_authorized_as_a_support_user
+    page.driver.basic_authorize(
+      ENV.fetch("SUPPORT_USERNAME", "test"),
+      ENV.fetch("SUPPORT_PASSWORD", "test")
+    )
+  end
+
+  def and_i_visit_the_test_users_section
+    visit support_interface_root_path
+    click_link "Test Users"
+  end
+
+  def and_i_create_a_new_user
+    click_button "Create new user"
+  end
+
+  def then_i_am_signed_in_as_that_user
+    expect(page).to have_content "Sign out"
+  end
+end
