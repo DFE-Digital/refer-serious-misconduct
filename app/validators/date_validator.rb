773686f88a61d81d8188090e014bf9c418920d85
Steve Laing <steve.laing@gmail.com>
2022-12-01 12:39:32 +0000

    Add a date validator with similar logic to existing concern

---
+++ app/validators/date_validator.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+class DateValidator < ActiveModel::EachValidator
+  WORDS_FOR_NUMBERS = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10,
+    eleven: 11,
+    twelve: 12
+  }.freeze
+
+  def validate_each(record, attribute, _value)
+    date_params_attribute = options[:date_params] || :date_params
+    date_params = record.send(date_params_attribute)
+    no_params = date_params.blank? || date_params.values.compact_blank.empty?
+
+    if no_params
+      return true if options[:required] == false
+
+      record.errors.add(attribute, :blank)
+      return false
+    end
+
+    date_fields =
+      [
+        date_params["#{attribute}(1i)"],
+        date_params["#{attribute}(2i)"],
+        date_params["#{attribute}(3i)"]
+      ].map { |f| f&.to_s&.strip }
+
+    # Use a struct instead of a date object to maintain what the user typed in,
+    # and not transform the fields into other data types like integers that
+    # Date.new is capable of accepting.
+    record.send(
+      "#{attribute}=",
+      Struct.new(:year, :month, :day).new(*date_fields)
+    )
+
+    year, month, day = date_fields.map { |f| word_to_number(f) }.map(&:to_i)
+    month = word_to_month(date_fields[1]) if month.zero? &&
+      date_fields[1].present?
+
+    if day.zero? && month.zero? && year.zero? && !options[:required]
+      record.errors.add(attribute, :blank)
+      return false
+    end
+
+    if day.zero?
+      record.errors.add(attribute, :missing_day)
+      return false
+    end
+
+    if month.zero?
+      record.errors.add(attribute, :missing_month)
+      return false
+    end
+
+    if year < 1000
+      record.errors.add(attribute, :missing_year)
+      return false
+    end
+
+    begin
+      record.send("#{attribute}=", Date.new(year, month, day))
+    rescue Date::Error
+      record.errors.add(attribute, :invalid)
+      return false
+    end
+
+    return true unless options[:date_of_birth]
+
+    if year > Time.zone.today.year
+      record.errors.add(attribute, :in_the_future)
+      return false
+    end
+
+    return true unless options[:date_of_birth]
+
+    if year < 1900
+      record.errors.add(attribute, :born_after_1900)
+      return false
+    end
+
+    if record.send(attribute) > 16.years.ago
+      record.errors.add(attribute, :inclusion)
+      return false
+    end
+
+    true
+  end
+
+  def word_to_number(field)
+    return field if field.is_a? Integer
+
+    WORDS_FOR_NUMBERS[field.downcase.to_sym] || field
+  end
+
+  # Attempts to convert Jan, Feb, etc to month numbers. Returns 0 otherwise.
+  def word_to_month(raw_month)
+    begin
+      month = Date.parse(raw_month).month
+    rescue Date::Error
+      month = 0
+    end
+    month
+  end
+end
---
+++ spec/validators/date_validator_spec.rb
@@ -0,0 +1,133 @@
+require "rails_helper"
+
+RSpec.describe DateValidator do
+  def stub_validatable_class(name, options: true)
+    stub_const(name, Class.new).class_eval do
+      include ActiveModel::Validations
+      attr_accessor :date_params, :the_date
+      validates :the_date, date: options
+    end
+  end
+
+  subject(:model) do
+    stub_validatable_class("Validatable")
+    Validatable.new
+  end
+
+  let(:date_params) do
+    { "the_date(1i)" => "2021", "the_date(2i)" => "12", "the_date(3i)" => "25" }
+  end
+
+  before { model.date_params = date_params }
+
+  context "with valid date params" do
+    it { is_expected.to be_valid }
+
+    it "sets the date on the validated attribute" do
+      model.valid?
+      expect(model.the_date).to eq(Date.new(2021, 12, 25))
+    end
+  end
+
+  context "with no date params" do
+    let(:date_params) { nil }
+
+    it { is_expected.to be_invalid }
+  end
+
+  context "with invalid date params" do
+    let(:date_params) do
+      { "the_date(1i)" => "-1", "the_date(2i)" => "-1", "the_date(3i)" => "-1" }
+    end
+
+    it { is_expected.to be_invalid }
+  end
+
+  context "with valid worded params" do
+    let(:date_params) do
+      {
+        "the_date(1i)" => "1990",
+        "the_date(2i)" => "September",
+        "the_date(3i)" => "seven"
+      }
+    end
+
+    it { is_expected.to be_valid }
+  end
+
+  context "with date_of_birth option" do
+    subject(:model) do
+      stub_validatable_class("DobValidatable", options: { date_of_birth: true })
+      DobValidatable.new
+    end
+
+    context "with valid date params" do
+      let(:date_params) do
+        {
+          "the_date(1i)" => "1990",
+          "the_date(2i)" => "12",
+          "the_date(3i)" => "25"
+        }
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context "with date of birth params in future" do
+      let(:date_params) do
+        {
+          "the_date(1i)" => 1.year.since.year,
+          "the_date(2i)" => "12",
+          "the_date(3i)" => "25"
+        }
+      end
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "with min age date of birth params" do
+      let(:date_params) do
+        {
+          "the_date(1i)" => 15.years.ago.year,
+          "the_date(2i)" => "12",
+          "the_date(3i)" => "25"
+        }
+      end
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "with max age date of birth params" do
+      let(:date_params) do
+        {
+          "the_date(1i)" => "1899",
+          "the_date(2i)" => "12",
+          "the_date(3i)" => "25"
+        }
+      end
+
+      it { is_expected.to be_invalid }
+    end
+  end
+
+  context "with required: false option" do
+    subject(:model) do
+      stub_validatable_class("ReqValidatable", options: { required: false })
+      ReqValidatable.new
+    end
+
+    context "with empty date params" do
+      let(:date_params) do
+        { "the_date(1i)" => "", "the_date(2i)" => "", "the_date(3i)" => "" }
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context "with no date params" do
+      let(:date_params) { nil }
+
+      it { is_expected.to be_valid }
+    end
+  end
+end
