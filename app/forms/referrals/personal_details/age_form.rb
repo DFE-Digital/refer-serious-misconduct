0000fc1b5fa8b8544d2ffae35ccd51a797278503
Steve Laing <steve.laing@gmail.com>
2022-12-01 12:40:23 +0000

    Use DateValidator in AgeForm

--- app/forms/referrals/personal_details/age_form.rb
+++ app/forms/referrals/personal_details/age_form.rb
@@ -3,39 +3,33 @@ module Referrals
   module PersonalDetails
     class AgeForm
       include ActiveModel::Model
-      include ValidatedDate
 
       attr_accessor :date_params, :referral
+      attr_reader :age_known
       attr_writer :date_of_birth
 
-      attr_reader :age_known
-
       validates :age_known, inclusion: { in: [true, false] }
+      validates :date_of_birth,
+                date: {
+                  date_of_birth: true
+                },
+                if: -> { age_known }
 
       def age_known=(value)
         @age_known = ActiveModel::Type::Boolean.new.cast(value)
       end
 
       def date_of_birth
-        @date_of_birth ||= referral.date_of_birth
+        @date_of_birth ||= referral&.date_of_birth
       end
 
       def save
         return false if invalid?
 
-        referral.update(age_known:)
+        attrs = { age_known: }
+        attrs.merge!(date_of_birth:) if age_known
 
-        return true unless age_known
-
-        unless validated_date(
-                 attribute: :date_of_birth,
-                 date_of_birth: true,
-                 date_params:
-               )
-          return false
-        end
-
-        referral.update(age_known:, date_of_birth:)
+        referral.update(attrs)
       end
     end
   end
