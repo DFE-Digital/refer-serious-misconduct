2e5bfe88881d0f5ddbd92287f772eb3658682722
Steve Laing <steve.laing@gmail.com>
2022-12-01 12:42:16 +0000

    Use DateValidator in EmploymentStatusForm

--- app/forms/referrals/teacher_role/employment_status_form.rb
+++ app/forms/referrals/teacher_role/employment_status_form.rb
@@ -3,7 +3,6 @@ module Referrals
   module TeacherRole
     class EmploymentStatusForm
       include ActiveModel::Model
-      include ValidatedDate
 
       attr_accessor :date_params,
                     :referral,
@@ -20,20 +19,11 @@ module Referrals
                 inclusion: {
                   in: %w[resigned dismissed retired unknown]
                 },
-                if: -> { left_role? }
-      validates :date_params, presence: true, if: :left_role?
+                if: :left_role?
+      validates :role_end_date, date: { required: false }, if: :left_role?
 
       def save
         return false if invalid?
-
-        if date_has_values?(date_params) &&
-             !validated_date(
-               date_params:,
-               attribute: :role_end_date,
-               required: false
-             )
-          return false
-        end
 
         referral.update(
           employment_status:,
@@ -46,10 +36,6 @@ module Referrals
 
       def left_role?
         employment_status == "left_role"
-      end
-
-      def date_has_values?(params)
-        left_role? && params.values.compact_blank.any?
       end
     end
   end
