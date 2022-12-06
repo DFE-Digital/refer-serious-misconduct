fb34dcd9f6095eb94257cfe366a5a4df2d62925a
Theodor Vararu <theo@vararu.org>
2022-12-06 12:38:03 +0000

    Revert "Remove redundant return statement"
    This reverts commit 0d417fe7dbe2f0e04e58af8e38edf4ef42be7764.

--- app/controllers/concerns/redirect_if_feature_flag_inactive.rb
+++ app/controllers/concerns/redirect_if_feature_flag_inactive.rb
@@ -5,6 +5,6 @@ module RedirectIfFeatureFlagInactive
   def redirect_if_feature_flag_inactive(feature, path = start_path)
     return if FeatureFlags::FeatureFlag.active?(feature)
 
-    redirect_to(path)
+    redirect_to(path) && return
   end
 end
