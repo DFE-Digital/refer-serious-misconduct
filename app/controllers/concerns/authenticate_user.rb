aae9a316d1eb5c739032ccf4de1844cc1244b578
Theodor Vararu <theo@vararu.org>
2022-12-06 12:38:03 +0000

    Revert "Move common controller methods and before_actions into concerns"
    This reverts commit 961670b6bc8b1cfe57c3c8ee7735deb07c8ae781.

--- app/controllers/concerns/authenticate_user.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module AuthenticateUser
-  extend ActiveSupport::Concern
-
-  included do
-    before_action :authenticate_user!,
-                  if: -> { FeatureFlags::FeatureFlag.active?(:user_accounts) }
-  end
-end
