874e326576afb38ece227ff4d8c0aef821778ad4
Theodor Vararu <theo@vararu.org>
2022-12-06 12:38:03 +0000

    Revert "Add common eligibility screener controller"
    This reverts commit a76e3c5628f239413f8756014f70f321c30defe1.

--- app/controllers/eligibility_screener_controller.rb
+++
@@ -1,8 +0,0 @@
-class EligibilityScreenerController < ApplicationController
-  include AuthenticateUser
-  include StoreUserLocation
-  include EnforceQuestionOrder
-  include RedirectIfFeatureFlagInactive
-
-  before_action { redirect_if_feature_flag_inactive(:employer_form) }
-end
