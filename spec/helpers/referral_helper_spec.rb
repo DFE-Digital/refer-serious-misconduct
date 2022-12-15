ae4c52e6fccf5088633e2d5d6d64a8073027c7db
Steve Laing <steve.laing@gmail.com>
2022-12-15 10:44:58 +0000

    Add method to build referral subsection paths

--- spec/helpers/referral_helper_spec.rb
+++ spec/helpers/referral_helper_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe ReferralHelper, type: :helper do
   let(:evidence) { create(:referral_evidence, referral:) }
   let(:referral) { create(:referral) }
 
-  before { referral.evidences = evidences }
 
   describe "#evidence_categories_back_link" do
     subject(:link) { helper.evidence_categories_back_link(form) }
 
     let(:form) { Referrals::Evidence::CategoriesForm.new(referral:, evidence:) }
+
+    before { referral.evidences = evidences }
 
     context "with previous evidence" do
       let(:mock_session) do
@@ -41,6 +42,8 @@ RSpec.describe ReferralHelper, type: :helper do
   describe "#evidence_check_answers_link" do
     subject(:link) { helper.evidence_check_answers_link(referral) }
 
+    before { referral.evidences = evidences }
+
     context "with evidence" do
       let(:evidences) { [evidence] }
 
@@ -55,6 +58,50 @@ RSpec.describe ReferralHelper, type: :helper do
       let(:evidences) { [] }
 
       it { is_expected.to eq(edit_referral_evidence_start_path(referral)) }
+    end
+  end
+
+  describe "#subsection_path" do
+    it "returns the path for a referral subsection" do
+      subsection_link =
+        helper.subsection_path(referral:, subsection: :personal_details_age)
+      expect(subsection_link).to eq(
+        "/referrals/#{referral.id}/personal-details/age"
+      )
+    end
+
+    it "returns the public variant of the path for a referral subsection" do
+      allow(referral).to receive(:from_employer?).and_return(false)
+      subsection_link =
+        helper.subsection_path(referral:, subsection: :personal_details_name)
+      expect(subsection_link).to eq(
+        "/public-referrals/#{referral.id}/personal-details/name"
+      )
+    end
+
+    it "includes an action if present" do
+      subsection_link =
+        helper.subsection_path(
+          referral:,
+          subsection: :personal_details_age,
+          action: :edit
+        )
+      expect(subsection_link).to eq(
+        "/referrals/#{referral.id}/personal-details/age/edit"
+      )
+    end
+
+    it "includes a return to link" do
+      subsection_link =
+        helper.subsection_path(
+          referral:,
+          subsection: :personal_details_age,
+          action: :edit,
+          return_to: "/foo/bar"
+        )
+      expect(subsection_link).to eq(
+        "/referrals/#{referral.id}/personal-details/age/edit?return_to=#{CGI.escape("/foo/bar")}"
+      )
     end
   end
 end
