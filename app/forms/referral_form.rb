0e6c38d44bc08a7a7d0d9cddb60faa111c83e1cf
Malcolm Baig <git@baig.org>
2022-12-22 15:11:28 +0000

    Don't show employer form section items on public form hub page
    The task list on the public form hub page has fewer section items than
    the employer form. Add conditional checks to ReferralForm so that we
    only show those that are relevant.

--- app/forms/referral_form.rb
+++ app/forms/referral_form.rb
@@ -32,78 +32,99 @@ class ReferralForm
   end
 
   def about_you_section
-    ReferralSection.new(
-      1,
-      I18n.t("referral_form.about_you"),
-      [
-        ReferralSectionItem.new(
-          I18n.t("referral_form.your_details"),
-          edit_referral_referrer_name_path(referral),
-          referral.referrer_status
-        ),
-        ReferralSectionItem.new(
-          I18n.t("referral_form.your_organisation"),
-          edit_referral_organisation_name_path(referral),
-          referral.organisation_status
-        )
-      ]
-    )
+    ReferralSection
+      .new(1, I18n.t("referral_form.about_you"))
+      .tap do |section|
+        section.items = [
+          ReferralSectionItem.new(
+            I18n.t("referral_form.your_details"),
+            edit_referral_referrer_name_path(referral),
+            referral.referrer_status
+          )
+        ]
+
+        if referral.from_employer?
+          section.items.append(
+            ReferralSectionItem.new(
+              I18n.t("referral_form.your_organisation"),
+              edit_referral_organisation_name_path(referral),
+              referral.organisation_status
+            )
+          )
+        end
+      end
   end
 
   def about_the_person_you_are_referring_section
-    ReferralSection.new(
-      2,
-      I18n.t("referral_form.about_the_person_you_are_referring"),
-      [
-        ReferralSectionItem.new(
-          I18n.t("referral_form.personal_details"),
-          subsection_path(
-            referral:,
-            subsection: :personal_details_name,
-            action: :edit
-          ),
-          section_status(:personal_details_complete)
-        ),
-        ReferralSectionItem.new(
-          I18n.t("referral_form.contact_details"),
-          edit_referral_contact_details_email_path(referral),
-          section_status(:contact_details_complete)
-        ),
-        ReferralSectionItem.new(
-          I18n.t("referral_form.about_their_role"),
-          edit_referral_teacher_role_start_date_path(referral),
-          section_status(:teacher_role_complete)
+    ReferralSection
+      .new(2, I18n.t("referral_form.about_the_person_you_are_referring"))
+      .tap do |section|
+        section.items = [
+          ReferralSectionItem.new(
+            I18n.t("referral_form.personal_details"),
+            subsection_path(
+              referral:,
+              subsection: :personal_details_name,
+              action: :edit
+            ),
+            section_status(:personal_details_complete)
+          )
+        ]
+
+        if referral.from_employer?
+          section.items.append(
+            ReferralSectionItem.new(
+              I18n.t("referral_form.contact_details"),
+              edit_referral_contact_details_email_path(referral),
+              section_status(:contact_details_complete)
+            )
+          )
+        end
+
+        section.items.append(
+          ReferralSectionItem.new(
+            I18n.t("referral_form.about_their_role"),
+            edit_referral_teacher_role_start_date_path(referral),
+            section_status(:teacher_role_complete)
+          )
         )
-      ]
-    )
+      end
   end
 
   def the_allegation_section
-    ReferralSection.new(
-      3,
-      I18n.t("referral_form.the_allegation"),
-      [
-        ReferralSectionItem.new(
-          I18n.t("referral_form.details_of_the_allegation"),
-          edit_referral_allegation_details_path(referral),
-          section_status(:allegation_details_complete)
-        ),
-        ReferralSectionItem.new(
-          I18n.t("referral_form.previous_allegations"),
-          edit_referral_previous_misconduct_reported_path(referral),
-          referral.previous_misconduct_status
-        ),
-        ReferralSectionItem.new(
-          I18n.t("referral_form.evidence_and_supporting_information"),
-          path_for_section_status(
-            section_status(:evidence_details_complete),
-            edit_referral_evidence_start_path(referral),
-            edit_referral_evidence_check_answers_path(referral)
-          ),
-          section_status(:evidence_details_complete)
+    ReferralSection
+      .new(3, I18n.t("referral_form.the_allegation"))
+      .tap do |section|
+        section.items = [
+          ReferralSectionItem.new(
+            I18n.t("referral_form.details_of_the_allegation"),
+            edit_referral_allegation_details_path(referral),
+            section_status(:allegation_details_complete)
+          )
+        ]
+
+        if referral.from_employer?
+          section.items.append(
+            ReferralSectionItem.new(
+              I18n.t("referral_form.previous_allegations"),
+              edit_referral_previous_misconduct_reported_path(referral),
+              referral.previous_misconduct_status
+            )
+          )
+        end
+
+        section.items.append(
+          ReferralSectionItem.new(
+            I18n.t("referral_form.evidence_and_supporting_information"),
+            path_for_section_status(
+              section_status(:evidence_details_complete),
+              edit_referral_evidence_start_path(referral),
+              edit_referral_evidence_check_answers_path(referral)
+            ),
+            section_status(:evidence_details_complete)
+          )
         )
-      ]
-    )
+      end
   end
 
   def section_status(section_complete_method)
