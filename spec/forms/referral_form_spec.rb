c1a1076e841e91ff1ff185895cca3565071c0e40
Malcolm Baig <git@baig.org>
2022-12-22 15:11:28 +0000

    Don't show employer form section items on public form hub page
    The task list on the public form hub page has fewer section items than
    the employer form. Add conditional checks to ReferralForm so that we
    only show those that are relevant.

--- spec/forms/referral_form_spec.rb
+++ spec/forms/referral_form_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ReferralForm do
-  describe "save" do
+  describe "#save" do
     subject(:save) { form.save }
 
     let(:form) { described_class.new(referral:) }
@@ -13,6 +13,54 @@ RSpec.describe ReferralForm do
       let(:referral) { create(:referral, :complete) }
 
       it { is_expected.to be_truthy }
+    end
+  end
+
+  describe "#sections" do
+    subject(:sections) { form.sections }
+
+    let(:form) { described_class.new(referral:) }
+
+    context "when referral is from employer" do
+      let(:referral) { create(:referral) }
+
+      it "returns employer form section items" do
+        labels = sections.flat_map(&:items).pluck(:label)
+        expect(labels).to eq(
+          [
+            "Your details",
+            "Your organisation",
+            "Personal details",
+            "Contact details",
+            "About their role",
+            "Details of the allegation",
+            "Previous allegations",
+            "Evidence and supporting information"
+          ]
+        )
+      end
+    end
+
+    context "when referral is from member of public" do
+      let(:referral) do
+        create(
+          :referral,
+          eligibility_check: create(:eligibility_check, :public)
+        )
+      end
+
+      it "returns public form section items" do
+        labels = sections.flat_map(&:items).pluck(:label)
+        expect(labels).to eq(
+          [
+            "Your details",
+            "Personal details",
+            "About their role",
+            "Details of the allegation",
+            "Evidence and supporting information"
+          ]
+        )
+      end
     end
   end
 end
