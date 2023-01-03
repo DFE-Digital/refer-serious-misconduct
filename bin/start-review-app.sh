95748a0d4cfa7b6872a7f09889478c6c34a248ae
NickGraham101 <nick.graham101@googlemail.com>
2023-01-03 16:21:01 +0000

    Log environment variables

--- bin/start-review-app.sh
+++ bin/start-review-app.sh
@@ -3,5 +3,6 @@
 /usr/sbin/sshd
 export DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 # Get environment variables to show up in SSH session
+echo -e "Environment variables:\n$(printenv)"
 eval $(printenv | awk -F= '{print "export " "\""$1"\"""=""\""$2"\"" }' >> /etc/profile)
 bundle exec rails db:schema_load_or_migrate && bundle exec rails server -b 0.0.0.0
