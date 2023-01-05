6bb6748bc8ef0444b3eff685e091f5150dfc54ba
Felix Clack <felixclack@gmail.com>
2023-01-05 11:58:16 +0000

    Update the phone number question
    The designs have updated the phone number question copy for both the
    form and the error messages.
    
    See the design (here)[https://refer-misconduct-prototype.herokuapp.com/report/your-details/telephone].
    
    The changes here will be the same for both the employer and public
    forms.

--- spec/forms/referrer_phone_form_spec.rb
+++ spec/forms/referrer_phone_form_spec.rb
@@ -3,19 +3,44 @@ require "rails_helper"
 RSpec.describe ReferrerPhoneForm, type: :model do
   let(:referral) { build(:referral, referrer:) }
   let(:referrer) { build(:referrer) }
+  let(:phone) { "07700 123456" }
 
   describe "validations" do
-    subject { described_class.new(referral:) }
+    subject(:form) { described_class.new(referral:, phone:) }
 
     it { is_expected.to validate_presence_of(:referral) }
     it { is_expected.to validate_presence_of(:phone) }
+
+    context "when the phone number is blank" do
+      let(:phone) { "" }
+
+      it "adds an error" do
+        form.validate
+        expect(form.errors[:phone]).to eq(
+          ["Enter your phone number, like 07700 900 982"]
+        )
+      end
+    end
+
+    context "when the phone number is in an incorrect format" do
+      let(:phone) { "invalid" }
+
+      it { is_expected.to be_invalid }
+
+      it "adds an error" do
+        form.validate
+        expect(form.errors[:phone]).to eq(
+          ["Enter your phone number in the correct format"]
+        )
+      end
+    end
   end
 
   describe "#save" do
     subject(:save) { form.save }
 
     let(:form) { described_class.new(phone:, referral:) }
-    let(:phone) { "123456789" }
+    let(:phone) { "07700123456" }
     let(:referral) { build(:referral, referrer:) }
     let(:referrer) { build(:referrer) }
 
