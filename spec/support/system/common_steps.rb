3289ba6bf9c4ca53fe87d4aa614b202cfe3b6c3b
Felix Clack <felixclack@gmail.com>
2023-01-18 12:59:48 +0000

    Display a simple referral page for case workers
    As part of building the case worker management interface, we need a page
    that displays the details of a specific referral.
    
    To make it easier for us to work on this page concurrently, I'm shipping
    a very basic version that we can iterate on.

--- spec/support/system/common_steps.rb
+++ spec/support/system/common_steps.rb
@@ -3,6 +3,10 @@ module CommonSteps
     FeatureFlags::FeatureFlag.activate(:service_open)
   end
 
+  def and_staff_http_basic_is_active
+    FeatureFlags::FeatureFlag.activate(:staff_http_basic_auth)
+  end
+
   def and_i_am_signed_in
     @user = create(:user)
     sign_in(@user)
