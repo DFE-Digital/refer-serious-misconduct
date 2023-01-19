0704e1f56666f606704e4888eea295406311ba20
Felix Clack <felixclack@gmail.com>
2023-01-19 11:28:46 +0000

    Add all sections to case worker referral view
    This adds all the sections to the case worker referral view that are
    detailed in the current designs.
    
    There are some parts to this that aren't implemented yet.
    
    The referrer name is stored as a single field. For now I opted to split
    this value into first and last name methods so we can render the
    proposed UI.
    
    When the change to the way we collect this value happens, then these
    methods will continue to work and we won't need to change the referral
    view.
    
    Secondly, we don't collect the referrer's current employer address.
    
    Rather than guess at how we might do that, I left it unimplemented.

--- app/helpers/referral_helper.rb
+++ app/helpers/referral_helper.rb
@@ -30,4 +30,39 @@ module ReferralHelper
       "Incomplete"
     end
   end
+
+  def allegation_details(referral)
+    if referral.allegation_upload.attached?
+      [
+        "File:",
+        govuk_link_to(
+          referral.allegation_upload.filename,
+          rails_blob_path(referral.allegation_upload, disposition: "attachment")
+        )
+      ].join(" ").html_safe
+    elsif referral.allegation_details.present?
+      referral.allegation_details.truncate(150)
+    else
+      "Incomplete"
+    end
+  end
+
+  def previous_allegation_details(referral)
+    if referral.previous_misconduct_upload.attached?
+      [
+        "File:",
+        govuk_link_to(
+          referral.previous_misconduct_upload.filename,
+          rails_blob_path(
+            referral.previous_misconduct_upload,
+            disposition: "attachment"
+          )
+        )
+      ].join(" ").html_safe
+    elsif referral.previous_misconduct_details.present?
+      simple_format(referral.previous_misconduct_details)
+    else
+      "Incomplete"
+    end
+  end
 end
--- app/models/referrer.rb
+++ app/models/referrer.rb
@@ -1,6 +1,14 @@
 class Referrer < ApplicationRecord
   belongs_to :referral
 
+  def first_name
+    name.split(" ").first
+  end
+
+  def last_name
+    name.split(" ")[1]
+  end
+
   def completed?
     completed_at.present?
   end
