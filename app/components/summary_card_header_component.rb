5d2202e89647e7fa58b904c073c137d27679257c
Felix Clack <felixclack@gmail.com>
2023-01-27 10:19:24 +0000

    Update the check answers style
    The style of the summary list for all the check answers has changed in
    the latest designs.
    
    This change updates all the relevant pages.

--- app/components/summary_card_header_component.rb
+++
@@ -1,8 +0,0 @@
-class SummaryCardHeaderComponent < ViewComponent::Base
-  def initialize(title:, heading_level: 2, anchor: nil)
-    super
-    @title = title
-    @heading_level = heading_level
-    @anchor = anchor
-  end
-end
