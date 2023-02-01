ea383b00bfd17f81bc1b98fc81e791bd145b2ca8
Paul Odeon <mail@paulodeon.com>
2023-02-01 17:46:19 +0000

    Users with a submitted referral redirect to confirmation page after login

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def send_otp(user)
     @otp = Devise::Otp.derive_otp(user.secret_key)
+    Rails.logger.info "OTP: #{@otp}" if Rails.env.development?
     mailer_options = { to: user.email, subject: "Confirm your email address" }
 
     notify_email(mailer_options)
---
+++ spec/system/user_auth/user_with_completed_referral_signs_in_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.feature "User accounts" do
+  include CommonSteps
+
+  scenario "User with completed referral signs in" do
+    given_the_service_is_open
+    and_the_eligibility_screener_feature_is_active
+    and_the_referral_form_feature_is_active
+
+    when_i_visit_the_service
+    and_i_have_a_completed_referral
+    and_click_start_now
+    and_i_submit_my_email
+
+    when_i_provide_the_expected_otp
+    then_i_am_signed_in
+    then_i_see_the_confirmation_page
+  end
+
+  private
+
+  def and_i_have_a_completed_referral
+    @user = create(:user)
+    @referral = create(:referral, user: @user, submitted_at: 1.day.ago)
+  end
+
+  def and_click_start_now
+    click_on "Start now"
+  end
+
+  def and_i_submit_my_email
+    fill_in "user-email-field", with: @user.email
+    click_on "Continue"
+  end
+
+  def when_i_provide_the_expected_otp
+    perform_enqueued_jobs
+
+    user = User.find(@user.id)
+    expected_otp = Devise::Otp.derive_otp(user.secret_key)
+
+    email = ActionMailer::Base.deliveries.last
+    expect(email.body).to include expected_otp
+
+    fill_in "Enter your code", with: expected_otp
+    within("main") { click_on "Continue" }
+  end
+
+  def then_i_am_signed_in
+    within(".govuk-header") { expect(page).to have_content "Sign out" }
+  end
+
+  def then_i_see_the_confirmation_page
+    expect(page).to have_current_path("/referrals/#{@referral.id}/confirmation")
+    expect(page).to have_title("Referral sent")
+    expect(page).to have_content("Referral sent")
+  end
+end
