8e9f09b6dbcbc0f43377c7f086d5b894822d070e
Octavian DFE <>
2023-02-07 09:24:49 +0000

    Apply code review notes

--- spec/forms/organisation_form_spec.rb
+++ spec/forms/organisation_form_spec.rb
@@ -24,29 +24,4 @@ RSpec.describe OrganisationForm, type: :model do
       it { is_expected.to be_falsey }
     end
   end
-
-  describe "#complete" do
-    subject(:form_complete) { form.complete }
-
-    let(:form) { described_class.new(complete:, referral:) }
-    let(:organisation) { build(:organisation) }
-    let(:referral) { organisation.referral }
-
-    context "when the value of complete is nil" do
-      let(:complete) { nil }
-      let(:organisation) { build(:organisation) }
-
-      it "returns the value from the organisation" do
-        expect(form_complete).to eq(organisation.completed?)
-      end
-    end
-
-    context "when the value of complete is set" do
-      let(:complete) { true }
-
-      it "returns the value of complete" do
-        expect(complete).to eq(true)
-      end
-    end
-  end
 end
--- spec/forms/referrer_form_spec.rb
+++ spec/forms/referrer_form_spec.rb
@@ -12,28 +12,4 @@ RSpec.describe ReferrerForm, type: :model do
 
     it { is_expected.to validate_presence_of(:referrer) }
   end
-
-  describe "#complete" do
-    subject(:form_complete) { referrer_form.complete }
-
-    let(:complete) { nil }
-    let(:referrer) { build(:referrer) }
-    let(:referrer_form) { described_class.new(complete:, referrer:) }
-
-    context "when the value of complete is nil" do
-      let(:referrer) { build(:referrer) }
-
-      it "returns the value from referrer" do
-        expect(form_complete).to eq(referrer.completed?)
-      end
-    end
-
-    context "when the value of complete is set" do
-      let(:complete) { true }
-
-      it "returns the value of complete" do
-        expect(complete).to eq(true)
-      end
-    end
-  end
 end
