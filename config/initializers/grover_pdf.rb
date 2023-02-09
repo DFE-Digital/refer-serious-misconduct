1aca32a04c17999bca714ab23a0ec5241ab9777e
Graham Pengelly <graham@three-tier.com>
2023-02-09 17:02:25 +0000

    [1110] Add referral PDF template

---
+++ config/initializers/grover_pdf.rb
@@ -0,0 +1,11 @@
+Grover.configure do |config|
+  config.options = {
+    format: "A4",
+    margin: {
+      top: "2cm",
+      bottom: "2cm",
+      left: "1cm",
+      right: "1cm"
+    }
+  }
+end
