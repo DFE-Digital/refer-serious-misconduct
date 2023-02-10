ed53d8532ce5dd65d1f67d7f7beda79f83c27b5d
Graham Pengelly <graham@three-tier.com>
2023-02-10 14:03:30 +0000

    Remove PLPGSQL extension
    * Remove from the schema
    
    It's causing issues deploying review apps and we don't use it. This will
    get readded with local migrations if the local DB has it so we'll need
    to take care not to reintroduce it.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[7.0].define(version: 2023_02_06_144743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
