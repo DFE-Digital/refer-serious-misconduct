d4e7acca8228b83ee60b9335edc3a9f120a33c3e
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-02-17 12:29:46 +0000

    Add various improvements
    - add newline in referral submitted email
    - remove unused evidence categories code
    - remove unnecessary padding in manage referral view

--- spec/factories/referral_evidences.rb
+++ spec/factories/referral_evidences.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :referral_evidence do
     referral
-    categories_other { "conduct_behaviour_attitude" }
     document do
       Rack::Test::UploadedFile.new(
         Rails.root.join("spec/fixtures/files/upload1.pdf"),
