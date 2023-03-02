b23d71e367ff8b13dbfbe1f6110e0736139212aa
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-03-02 10:16:23 +0000

    Validate NINO format
    This only validates the format, the NINO can still be an invalid one,
    but in the correct format.

--- app/forms/referrals/personal_details/ni_number_form.rb
+++ app/forms/referrals/personal_details/ni_number_form.rb
@@ -4,14 +4,23 @@ module Referrals
     class NiNumberForm
       include ActiveModel::Model
 
-      attr_accessor :referral, :ni_number
-      attr_reader :ni_number_known
+      attr_accessor :referral
+      attr_reader :ni_number_known, :ni_number
 
-      validates :ni_number, presence: true, if: -> { ni_number_known }
+      validates :ni_number,
+                presence: true,
+                format: {
+                  with: /\A[a-z]{2}[0-9]{6}[a-d]{1}\Z/i
+                },
+                if: -> { ni_number_known }
       validates :ni_number_known, inclusion: { in: [true, false] }
 
       def ni_number_known=(value)
         @ni_number_known = ActiveModel::Type::Boolean.new.cast(value)
+      end
+
+      def ni_number=(value)
+        @ni_number = value&.gsub(/\s|-/, "")
       end
 
       def save
