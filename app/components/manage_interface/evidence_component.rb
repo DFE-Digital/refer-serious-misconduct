f97641fa67f69490c93e5524966e1f944d81e84f
Octavian DFE <>
2023-03-03 12:22:39 +0000

    Manage referrals - Evidence Details
    Add missing fields on the form visible inside the
    manage referrals - Evidence details section.

--- app/components/manage_interface/evidence_component.rb
+++ app/components/manage_interface/evidence_component.rb
@@ -1,7 +1,21 @@
 module ManageInterface
   class EvidenceComponent < ViewComponent::Base
     include ActiveModel::Model
+    include ReferralHelper
 
     attr_accessor :referral
+
+    def rows
+      [
+        {
+          key: {
+            text: "Is there anything to upload?"
+          },
+          value: {
+            text: referral.has_evidence ? "Yes" : "No"
+          }
+        }
+      ]
+    end
   end
 end
