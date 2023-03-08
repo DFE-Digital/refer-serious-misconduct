a461945ec7f29613f99806bbd67c72bf5948336e
Paul Odeon <mail@paulodeon.com>
2023-03-08 15:19:27 +0000

    Reorganise referral organisation form objects, controllers and views

---
+++ app/controllers/referrals/organisation/check_answers_controller.rb
@@ -0,0 +1,38 @@
+module Referrals
+  module Organisation
+    class CheckAnswersController < BaseController
+      def edit
+        @organisation =
+          current_referral.organisation || current_referral.build_organisation
+        @organisation_form =
+          CheckAnswersForm.new(
+            referral: current_referral,
+            complete: @organisation.complete
+          )
+      end
+
+      def update
+        @organisation_form =
+          CheckAnswersForm.new(
+            complete: organisation_params[:complete],
+            referral: current_referral
+          )
+
+        if @organisation_form.save
+          redirect_to [:edit, current_referral.routing_scope, current_referral]
+        else
+          @organisation = current_referral.organisation
+          render :edit
+        end
+      end
+
+      private
+
+      def organisation_params
+        params.require(:referrals_organisation_check_answers_form).permit(
+          :complete
+        )
+      end
+    end
+  end
+end
---
+++ app/forms/referrals/organisation/address_form.rb
@@ -0,0 +1,56 @@
+module Referrals
+  module Organisation
+    class AddressForm
+      include ActiveModel::Model
+
+      attr_accessor :referral
+      attr_writer :name, :street_1, :street_2, :city, :postcode
+
+      validates :name, presence: true
+      validates :street_1, presence: true
+      validates :city, presence: true
+      validates :postcode, presence: true
+      validates :referral, presence: true
+
+      validate :postcode_is_valid, if: -> { postcode.present? }
+
+      def city
+        @city ||= organisation&.city
+      end
+
+      def name
+        @name ||= organisation&.name
+      end
+
+      def organisation
+        @organisation ||= referral&.organisation || referral&.build_organisation
+      end
+
+      def postcode
+        @postcode ||= organisation&.postcode
+      end
+
+      def street_1
+        @street_1 ||= organisation&.street_1
+      end
+
+      def street_2
+        @street_2 ||= organisation&.street_2
+      end
+
+      def save
+        return false unless valid?
+
+        organisation.update(city:, name:, postcode:, street_1:, street_2:)
+      end
+
+      private
+
+      def postcode_is_valid
+        return if UKPostcode.parse(postcode).full_valid?
+
+        errors.add(:postcode, :invalid)
+      end
+    end
+  end
+end
