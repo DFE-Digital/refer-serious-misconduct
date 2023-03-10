fb8c1dae39b11d8db57b9da81f98afa45a5b59d1
Paul Odeon <mail@paulodeon.com>
2023-03-10 13:06:13 +0000

    Store declaration content on submit

---
+++ app/lib/declaration_renderer.rb
@@ -0,0 +1,15 @@
+class DeclarationRenderer
+  include ActionView::Helpers::SanitizeHelper
+
+  def render
+    strip_tags(rendered_declaration_partial)
+      .lines
+      .reject(&:blank?)
+      .each(&:strip!)
+      .join("\n")
+  end
+
+  def rendered_declaration_partial
+    ApplicationController.renderer.new.render(partial: "shared/declaration")
+  end
+end
---
+++ db/migrate/20230310125912_add_declaration_to_referrals.rb
@@ -0,0 +1,5 @@
+class AddDeclarationToReferrals < ActiveRecord::Migration[7.0]
+  def change
+    add_column :referrals, :declaration, :text
+  end
+end
