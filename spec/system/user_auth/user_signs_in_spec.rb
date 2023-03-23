eb1cf4ef1cbfeefa6ff50aeab0521f9a9a886158
Paul Odeon <mail@paulodeon.com>
2023-03-23 12:16:23 +0000

    Strip spaces in OTP input

--- spec/system/user_auth/user_signs_in_spec.rb
+++ spec/system/user_auth/user_signs_in_spec.rb
@@ -23,6 +23,14 @@ RSpec.feature "User accounts" do
     and_i_am_not_prompted_to_sign_in_again
     and_my_otp_state_is_reset
 
+    when_i_am_signed_out
+    when_i_visit_the_root_page
+    and_choose_continue_referral
+    then_i_should_see_the_sign_in_page
+    and_i_submit_my_email
+    when_i_provide_the_expected_otp_with_surrounding_spaces
+    then_i_am_signed_in
+
     when_i_have_a_referral_in_progress
     and_i_sign_out
     when_i_sign_back_in
@@ -93,6 +101,19 @@ RSpec.feature "User accounts" do
     expect(email.body).to include expected_otp
 
     fill_in "Confirmation code", with: expected_otp
+    within("main") { click_on "Continue" }
+  end
+
+  def when_i_provide_the_expected_otp_with_surrounding_spaces
+    perform_enqueued_jobs
+
+    user = User.find_by(email: "test@example.com")
+    expected_otp = Devise::Otp.derive_otp(user.secret_key)
+
+    email = ActionMailer::Base.deliveries.last
+    expect(email.body).to include expected_otp
+
+    fill_in "Confirmation code", with: " #{expected_otp} "
     within("main") { click_on "Continue" }
   end
 
