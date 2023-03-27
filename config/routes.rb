9ac1738dc53f6b5a06b8ff918a4cd4036bbd4705
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-03-27 15:33:53 +0100

    Hide show user referral page before submission

--- config/routes.rb
+++ config/routes.rb
@@ -250,7 +250,7 @@ Rails.application.routes.draw do
 
   scope via: :all do
     get "/403", to: "errors#forbidden", as: :forbidden
-    get "/404", to: "errors#not_found"
+    get "/404", to: "errors#not_found", as: :not_found
     get "/422", to: "errors#unprocessable_entity"
     get "/429", to: "errors#too_many_requests"
     get "/500", to: "errors#internal_server_error"
