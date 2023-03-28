6ad0b0937abb74d6f146ca46c9a5beb452e6aaa2
Paul Odeon <mail@paulodeon.com>
2023-03-28 11:19:42 +0100

    Refactor and add tests for the Evidence summary view

--- app/components/evidence_component.rb
+++ app/components/evidence_component.rb
@@ -1,54 +1,28 @@
 class EvidenceComponent < ViewComponent::Base
   include ActiveModel::Model
   include ReferralHelper
-  include ComponentHelper
 
   attr_accessor :referral
 
   def rows
-    items = [anything_to_upload_row, evidence_row].compact
-    referral.submitted? ? remove_actions(items) : items
+    summary_rows [anything_to_upload_row, evidence_row].compact
   end
 
   private
 
   def anything_to_upload_row
     {
-      actions: [
-        {
-          text: "Change",
-          href: polymorphic_path([:edit, referral.routing_scope, referral, :evidence_start], return_to: return_to_path),
-          visually_hidden_text: "if you have anything to upload"
-        }
-      ],
-      key: {
-        text: "Do you have anything to upload?"
-      },
-      value: {
-        text: nullable_boolean_to_s(referral.has_evidence)
-      }
+      label: "Do you have anything to upload?",
+      visually_hidden_text: "if you have anything to upload",
+      value: referral.has_evidence,
+      path: :evidence_start
     }
   end
 
   def evidence_row
     return unless referral.has_evidence?
 
-    {
-      actions: [
-        {
-          text: "Change",
-          href:
-            polymorphic_path([:edit, referral.routing_scope, referral, :evidence_uploaded], return_to: return_to_path),
-          visually_hidden_text: "uploaded evidence"
-        }
-      ],
-      key: {
-        text: "Uploaded evidence"
-      },
-      value: {
-        text: evidence_text
-      }
-    }
+    { label: "Uploaded evidence", value: evidence_text, path: :evidence_uploaded }
   end
 
   def evidence_text
