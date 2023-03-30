7a978ca5f8518a9f2486b987b1d85dd902c047a6
Octavian DFE <unknown>
2023-03-30 11:45:13 +0100

    Refactor the Contact Details Component

--- app/components/contact_details_component.rb
+++ app/components/contact_details_component.rb
@@ -7,94 +7,58 @@ class ContactDetailsComponent < ViewComponent::Base
   attr_accessor :referral
 
   def rows
-    items = [email_known_row]
-    items.push(email_row) if referral.email_known
-    items.push(phone_number_known_row)
-    items.push(phone_number_row) if referral.phone_known
-    items.push(address_known_row)
-    items.push(address_row) if referral.address_known
-    referral.submitted? ? remove_actions(items) : items
+    summary_rows [
+                   email_known_row,
+                   email_row,
+                   phone_number_known_row,
+                   phone_number_row,
+                   address_known_row,
+                   address_row
+                 ].compact
   end
 
-  def path_for(part)
-    [:edit, referral.routing_scope, referral, :contact_details, part, { return_to: }]
-  end
-
-  def return_to
-    polymorphic_path([:edit, referral.routing_scope, referral, :contact_details_check_answers])
-  end
+  private
 
   def email_known_row
     {
-      actions: [{ text: "Change", href: path_for(:email), visually_hidden_text: "if you know their email address" }],
-      key: {
-        text: "Do you know their email address?"
-      },
-      value: {
-        text: nullable_boolean_to_s(referral.email_known)
-      }
+      label: "Do you know their email address?",
+      value: referral.email_known,
+      visually_hidden_text: "if you know their email address",
+      path: :contact_details_email
     }
   end
 
   def email_row
     {
-      actions: [{ text: "Change", href: path_for(:email), visually_hidden_text: "email address" }],
-      key: {
-        text: "Email address"
-      },
-      value: {
-        text: referral.email_known ? referral.email_address : "Not known"
-      }
+      label: "Email address",
+      value: referral.email_known ? referral.email_address : "Not known",
+      path: :contact_details_email
     }
   end
 
   def phone_number_known_row
     {
-      actions: [{ text: "Change", href: path_for(:telephone), visually_hidden_text: "if you know their phone number" }],
-      key: {
-        text: "Do you know their phone number?"
-      },
-      value: {
-        text: nullable_boolean_to_s(referral.phone_known)
-      }
+      label: "Do you know their phone number?",
+      value: referral.phone_known,
+      visually_hidden_text: "if you know their phone number",
+      path: :contact_details_telephone
     }
   end
 
   def phone_number_row
-    {
-      actions: [{ text: "Change", href: path_for(:telephone), visually_hidden_text: "phone number" }],
-      key: {
-        text: "Phone number"
-      },
-      value: {
-        text: referral.phone_number
-      }
-    }
+    { label: "Phone number", value: referral.phone_number, path: :contact_details_telephone }
   end
 
   def address_known_row
     {
-      actions: [
-        { text: "Change", href: path_for(:address_known), visually_hidden_text: "if you know their home address" }
-      ],
-      key: {
-        text: "Do you know their home address?"
-      },
-      value: {
-        text: nullable_boolean_to_s(referral.address_known)
-      }
+      label: "Do you know their home address?",
+      value: referral.address_known,
+      visually_hidden_text: "if you know their home address",
+      path: :contact_details_address_known
     }
   end
 
   def address_row
-    {
-      actions: [{ text: "Change", href: path_for(:address), visually_hidden_text: "home address" }],
-      key: {
-        text: "Home address"
-      },
-      value: {
-        text: nullable_value_to_s(address(referral).presence)
-      }
-    }
+    { label: "Home address", value: address(referral).presence, path: :contact_details_address }
   end
 end
