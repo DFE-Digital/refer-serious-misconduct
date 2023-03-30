c963a5d57b31645f0f3c5aaafc3c3a6aacafad8d
Paul Odeon <mail@paulodeon.com>
2023-03-30 10:25:32 +0100

    Refactor and add tests to the organisation summary view

---
+++ spec/components/organisation_component_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe OrganisationComponent, type: :component do
+  subject(:component) { described_class.new(referral:) }
+
+  let(:organisation) { referral.organisation }
+
+  before { render_inline(component) }
+
+  context "with an employer referral" do
+    let(:referral) { create(:referral, :complete) }
+
+    it "renders the organisation row" do
+      expect(page).to have_css("dt", text: "Your organisation")
+
+      expect(page).to have_css("dd", text: organisation.name)
+      expect(page).to have_css("dd", text: organisation.street_1)
+      expect(page).to have_css("dd", text: organisation.street_2)
+      expect(page).to have_css("dd", text: organisation.city)
+      expect(page).to have_css("dd", text: organisation.postcode)
+
+      expect(page).to have_link "Change", href: "/referrals/#{referral.id}/organisation/address/edit?return_to=%2F"
+    end
+  end
+end
