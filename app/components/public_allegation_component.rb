388ae968edbd470687bf6be897b0a0c8acd0f94e
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-04-03 17:47:49 +0100

    Refactor PublicAllegationComponent

--- app/components/public_allegation_component.rb
+++ app/components/public_allegation_component.rb
@@ -1,9 +1,37 @@
 class PublicAllegationComponent < ViewComponent::Base
   include ActiveModel::Model
-  include ComponentHelper
   include ReferralHelper
 
   attr_accessor :referral
+
+  def rows
+    summary_rows [
+                   allegation_details_format_row,
+                   allegation_details_description_row,
+                   allegation_details_considerations_row
+                 ].compact
+  end
+
+  def allegation_details_format_row
+    {
+      label: "How do you want to give details about the allegation?",
+      visually_hidden_text: "how you want to give details about the allegation",
+      value: allegation_details_format(referral),
+      path: :allegation_details
+    }
+  end
+
+  def allegation_details_description_row
+    { label: "Description of the allegation", value: allegation_details(referral), path: :allegation_details }
+  end
+
+  def allegation_details_considerations_row
+    {
+      label: "Details about how this complaint has been considered",
+      value: simple_format(nullable_value_to_s(referral.allegation_consideration_details.presence)),
+      path: :allegation_considerations
+    }
+  end
 
   def allegation_check_answers_form
     @allegation_check_answers_form ||= Referrals::Allegation::CheckAnswersForm.new(referral:)
@@ -20,57 +48,5 @@ class PublicAllegationComponent < ViewComponent::Base
   def file_link
     upload = referral.allegation_upload
     govuk_link_to(upload.filename, rails_blob_path(upload, disposition: "attachment"))
-  end
-
-  def rows
-    items = [
-      {
-        actions: [
-          {
-            text: "Change",
-            href: edit_public_referral_allegation_details_path(referral, return_to: return_to_path),
-            visually_hidden_text: "how you want to give details about the allegation"
-          }
-        ],
-        key: {
-          text: "How do you want to give details about the allegation?"
-        },
-        value: {
-          text: allegation_details_format(referral)
-        }
-      },
-      {
-        actions: [
-          {
-            text: "Change",
-            href: edit_public_referral_allegation_details_path(referral, return_to: return_to_path),
-            visually_hidden_text: "description of the allegation"
-          }
-        ],
-        key: {
-          text: "Description of the allegation"
-        },
-        value: {
-          text: allegation_details(referral)
-        }
-      },
-      {
-        actions: [
-          {
-            text: "Change",
-            href: edit_public_referral_allegation_considerations_path(referral, return_to: return_to_path),
-            visually_hidden_text: "details about how this complaint has been considered"
-          }
-        ],
-        key: {
-          text: "Details about how this complaint has been considered"
-        },
-        value: {
-          text: simple_format(nullable_value_to_s(referral.allegation_consideration_details.presence))
-        }
-      }
-    ]
-
-    referral.submitted? ? remove_actions(items) : items
   end
 end
---
+++ spec/components/public_allegation_component_spec.rb
@@ -0,0 +1,83 @@
+require "rails_helper"
+require "action_view"
+
+RSpec.describe PublicAllegationComponent, type: :component do
+  subject(:component) { described_class.new(referral:) }
+
+  let(:referrer) { referral.referrer }
+
+  let(:row_labels) { component.rows.map { |row| row.dig(:key, :text) } }
+  let(:row_values) { component.rows.map { |row| row.dig(:value, :text) }.map(&:strip) }
+  let(:row_values_sanitized) { row_values.map { |value| ActionView::Base.full_sanitizer.sanitize(value) } }
+  let(:row_links) { component.rows.map { |row| row.dig(:actions, :href) } }
+  let(:allegation_format) { nil }
+  let(:allegation_details) { nil }
+  let(:allegation_upload) { nil }
+  let(:allegation_consideration_details) { nil }
+
+  let(:referral) do
+    create(
+      :referral,
+      :complete,
+      :public,
+      allegation_format:,
+      allegation_details:,
+      allegation_upload:,
+      allegation_consideration_details:
+    )
+  end
+
+  before { render_inline(component) }
+
+  it "renders the labels" do
+    expect(row_labels).to eq(
+      [
+        "How do you want to give details about the allegation?",
+        "Description of the allegation",
+        "Details about how this complaint has been considered"
+      ]
+    )
+  end
+
+  it "renders the values" do
+    expect(row_values_sanitized).to eq(["Not answered", "Not answered", "Not answered"])
+  end
+
+  it "renders the Change links" do
+    expect(row_links).to eq(
+      [
+        "/public-referrals/#{referral.id}/allegation/details/edit?return_to=%2F",
+        "/public-referrals/#{referral.id}/allegation/details/edit?return_to=%2F",
+        "/public-referrals/#{referral.id}/allegation/considerations/edit?return_to=%2F"
+      ]
+    )
+  end
+
+  context "with a description" do
+    let(:allegation_format) { "details" }
+    let(:allegation_details) { "They were rude to a child" }
+
+    it "renders the correct values" do
+      expect(row_values_sanitized).to eq(["Describe the allegation", "They were rude to a child", "Not answered"])
+    end
+  end
+
+  context "with an upload" do
+    let(:allegation_format) { "upload" }
+    let(:allegation_upload) do
+      Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/files/upload1.pdf"), "application/pdf")
+    end
+
+    it "renders the correct values" do
+      expect(row_values_sanitized).to eq(["Upload file", "upload1.pdf", "Not answered"])
+    end
+  end
+
+  context "with a consideration" do
+    let(:allegation_consideration_details) { "Please take into consideration" }
+
+    it "renders the correct values" do
+      expect(row_values_sanitized).to eq(["Not answered", "Not answered", "Please take into consideration"])
+    end
+  end
+end
