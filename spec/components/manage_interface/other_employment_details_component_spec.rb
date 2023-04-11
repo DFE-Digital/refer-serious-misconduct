ae6fd121a0f5d716d5a22e2c3f12ae60ca4ad541
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-04-11 14:04:36 +0100

    Refactor ManageInterface::OtherEmploymentDetailsComponent

---
+++ spec/components/manage_interface/other_employment_details_component_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe ManageInterface::OtherEmploymentDetailsComponent, type: :component do
+  subject(:component) { described_class.new(referral:) }
+
+  let(:row_labels) { component.rows.map { |row| row.dig(:key, :text) } }
+  let(:row_values) { component.rows.map { |row| row.dig(:value, :text) }.map(&:strip) }
+  let(:row_values_sanitized) do
+    row_values.map { |value| ActionView::Base.full_sanitizer.sanitize(value) }
+  end
+  let(:referral) { create(:referral, :employer_complete) }
+
+  before { render_inline(component) }
+
+  it "renders the correct labels" do
+    expect(row_labels).to eq(["Organisation"])
+  end
+
+  it "renders the correct values" do
+    expect(row_values_sanitized).to eq(
+      ["Different School2 Different StreetSame RoadExample TownAB1 2CD"]
+    )
+  end
+end
