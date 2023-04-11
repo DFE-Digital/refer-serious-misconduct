95b23037219f9bf37ca3ad169917e9e0c2f466d5
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-04-11 10:31:30 +0100

    Refactor ManageInterface::EvidenceComponent

---
+++ spec/components/manage_interface/evidence_component_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe ManageInterface::EvidenceComponent, type: :component do
+  subject(:component) { described_class.new(referral:) }
+
+  let(:row_labels) { component.rows.map { |row| row.dig(:key, :text) } }
+  let(:row_values) { component.rows.map { |row| row.dig(:value, :text) }.map(&:strip) }
+
+  before { render_inline(component) }
+
+  context "with a referral with evidence" do
+    let(:referral) { create(:referral, :employer_complete) }
+
+    it "doesn't render any rows" do
+      expect(component.rows).to be_nil
+    end
+  end
+
+  context "with a referral without evidence" do
+    let(:referral) { create(:referral) }
+
+    it "renders the correct labels" do
+      expect(row_labels).to eq(["Is there anything to upload?"])
+    end
+
+    it "renders the correct values" do
+      expect(row_values).to eq(["No"])
+    end
+  end
+end
