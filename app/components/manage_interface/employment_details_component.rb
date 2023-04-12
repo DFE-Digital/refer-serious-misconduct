b9bbb5d4031c1231f9b89ccfe9998ebbc4606cbf
Octavian DFE <unknown>
2023-04-12 18:21:10 +0100

    Refactor ManageInterface::EmploymentDetails component

--- app/components/manage_interface/employment_details_component.rb
+++ app/components/manage_interface/employment_details_component.rb
@@ -7,146 +7,15 @@ module ManageInterface
     attr_accessor :referral
 
     def rows
-      rows = [
-        { key: { text: "Job title" }, value: { text: referral.job_title || "Not known" } },
-        {
-          key: {
-            text: "How where details about their main duties given?"
-          },
-          value: {
-            text: duties_format(referral)
-          }
-        },
-        { key: { text: "Main duties" }, value: { text: duties_details(referral) } }
-      ]
-
-      if referral.from_member_of_public?
-        if !referral.organisation_address_known
-          rows.push(
-            {
-              key: {
-                text:
-                  "Do you know the name and address of the organisation where the alleged misconduct took place?"
-              },
-              value: {
-                text: "No"
-              }
-            }
-          )
-        else
-          rows.push(
-            {
-              key: {
-                text: "Name and address of the organisation where the alleged misconduct took place"
-              },
-              value: {
-                text: referral_organisation_address(referral)
-              }
-            }
-          )
-        end
-      end
-
-      return rows unless referral.from_employer?
-
-      if !referral.same_organisation
-        rows.push(
-          {
-            key: {
-              text:
-                "Were they employed at the same organisation as you at the time of the alleged misconduct?"
-            },
-            value: {
-              text: "No"
-            }
-          }
-        )
-      else
-        rows.push(organisation_row)
-      end
-
-      if !referral.role_start_date_known
-        rows.push(
-          { key: { text: "Do you know when they started the job?" }, value: { text: "No" } }
-        )
-      else
-        rows.push(
-          {
-            key: {
-              text: "Job start date"
-            },
-            value: {
-              text: referral.role_start_date&.to_fs(:long_ordinal_uk)
-            }
-          }
-        )
-      end
-
-      if employment_status(referral) == "No"
-        rows.push(
-          {
-            key: {
-              text:
-                "Are they still employed at the organisation where the alleged misconduct took place?"
-            },
-            value: {
-              text: "No"
-            }
-          }
-        )
-
-        if !referral.role_end_date_known
-          rows.push({ key: { text: "Do you know when they left the job?" }, value: { text: "No" } })
-        else
-          rows.push(
-            {
-              key: {
-                text: "Job end date"
-              },
-              value: {
-                text: referral.role_end_date&.to_fs(:long_ordinal_uk)
-              }
-            }
-          )
-        end
-
-        rows.push(
-          {
-            key: {
-              text: "Reason they left the job"
-            },
-            value: {
-              text: referral.reason_leaving_role&.humanize
-            }
-          }
-        )
-
-        if working_somewhere_else != "Yes"
-          rows.push(
-            {
-              key: {
-                text: "Are they employed somewhere else?"
-              },
-              value: {
-                text: working_somewhere_else
-              }
-            }
-          )
-        elsif !referral.work_location_known
-          rows.push(
-            {
-              key: {
-                text: "Do you know the name and address of the organisation where they’re employed?"
-              },
-              value: {
-                text: "No"
-              }
-            }
-          )
-        end
-      end
-
-      rows
+      summary_rows [
+                     job_title_row,
+                     details_about_main_duties_row,
+                     main_duties_row,
+                     organisation_address_where_misconduct_occurred_row,
+                     organisation_address_where_employed_row,
+                     role_start_date_row,
+                     employment_status_row
+                   ].flatten.compact
     end
 
     def title
@@ -155,22 +24,100 @@ module ManageInterface
 
     private
 
-    def organisation_row
-      {
-        key: {
-          text: "Organisation"
-        },
-        value: {
-          text:
-            (
-              if referral.same_organisation?
-                organisation_address(referral.organisation)
-              else
-                referral_organisation_address(referral)
-              end
-            )
+    def job_title_row
+      { label: "Job title", value: referral.job_title || "Not known" }
+    end
+
+    def details_about_main_duties_row
+      { label: "How where details about their main duties given?", value: duties_format(referral) }
+    end
+
+    def main_duties_row
+      { label: "Main duties", value: duties_details(referral) }
+    end
+
+    def organisation_address_where_misconduct_occurred_row
+      return unless referral.from_member_of_public?
+
+      if referral.organisation_address_known
+        {
+          label: "Name and address of the organisation where the alleged misconduct took place",
+          value: referral_organisation_address(referral)
         }
+      else
+        {
+          label:
+            "Do you know the name and address of the organisation where the alleged misconduct took place?",
+          value: "No"
+        }
+      end
+    end
+
+    def organisation_address_where_employed_row
+      return unless referral.from_employer?
+
+      if referral.same_organisation?
+        { label: "Organisation", value: organisation_address(referral.organisation) }
+      else
+        {
+          label:
+            "Were they employed at the same organisation as you at the time of the alleged misconduct?",
+          value: "No"
+        }
+      end
+    end
+
+    def role_start_date_row
+      return unless referral.from_employer?
+
+      if referral.role_start_date_known
+        { label: "Job start date", value: referral.role_start_date&.to_fs(:long_ordinal_uk) }
+      else
+        { label: "Do you know when they started the job?", value: "No" }
+      end
+    end
+
+    def employment_status_row
+      return unless referral.from_employer?
+      return if employment_status(referral) != "No"
+
+      [
+        not_employed_anymore_row,
+        role_end_date_row,
+        reason_for_leaving_row,
+        working_somewhere_else_row
+      ]
+    end
+
+    def not_employed_anymore_row
+      {
+        label:
+          "Are they still employed at the organisation where the alleged misconduct took place?",
+        value: "No"
       }
+    end
+
+    def role_end_date_row
+      if referral.role_end_date_known
+        { label: "Job end date", value: referral.role_end_date&.to_fs(:long_ordinal_uk) }
+      else
+        { label: "Do you know when they left the job?", value: "No" }
+      end
+    end
+
+    def reason_for_leaving_row
+      { label: "Reason they left the job", value: referral.reason_leaving_role&.humanize }
+    end
+
+    def working_somewhere_else_row
+      if working_somewhere_else != "Yes"
+        { label: "Are they employed somewhere else?", value: working_somewhere_else }
+      elsif !referral.work_location_known
+        {
+          label: "Do you know the name and address of the organisation where they’re employed?",
+          value: "No"
+        }
+      end
     end
 
     def working_somewhere_else
