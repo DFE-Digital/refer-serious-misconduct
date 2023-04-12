8fdcf2128a0f26ab429311bbf4777f69f17fb8e7
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-04-12 14:10:23 +0100

    Refactor ManageInterface::PersonalDetailsComponent

--- app/components/manage_interface/personal_details_component.rb
+++ app/components/manage_interface/personal_details_component.rb
@@ -1,108 +1,110 @@
 module ManageInterface
   class PersonalDetailsComponent < ViewComponent::Base
     include ActiveModel::Model
+    include ReferralHelper
     include AddressHelper
 
     attr_accessor :referral
 
     def rows
-      rows = [
-        { key: { text: "First name" }, value: { text: referral.first_name } },
-        { key: { text: "Last name" }, value: { text: referral.last_name } }
-      ]
-
-      if referral.name_has_changed == "no"
-        rows.push(
-          {
-            key: {
-              text: "Do you know them by any other name?"
-            },
-            value: {
-              text: referral.name_has_changed.humanize
-            }
-          }
-        )
-      elsif referral.previous_name.present?
-        rows.push({ key: { text: "Other name" }, value: { text: referral.previous_name } })
-      end
-
-      return rows unless referral.from_employer?
-
-      if !referral.age_known
-        rows.push({ key: { text: "Do you know their date of birth?" }, value: { text: "No" } })
-      else
-        rows.push(
-          {
-            key: {
-              text: "Date of birth"
-            },
-            value: {
-              text: referral.date_of_birth&.to_fs(:long_ordinal_uk)
-            }
-          }
-        )
-      end
-
-      if !referral.email_known
-        rows.push({ key: { text: "Do you know their email address?" }, value: { text: "No" } })
-      else
-        rows.push({ key: { text: "Email address" }, value: { text: referral.email_address } })
-      end
-
-      if !referral.phone_known
-        rows.push({ key: { text: "Do you know their phone number?" }, value: { text: "No" } })
-      else
-        rows.push({ key: { text: "Phone number" }, value: { text: referral.phone_number } })
-      end
-
-      if !referral.address_known
-        rows.push({ key: { text: "Do you know their address?" }, value: { text: "No" } })
-      else
-        rows.push({ key: { text: "Address" }, value: { text: address(referral) } })
-      end
-
-      if !referral.ni_number_known
-        rows.push(
-          { key: { text: "Do you know their National Insurance number?" }, value: { text: "No" } }
-        )
-      else
-        rows.push(
-          { key: { text: "National Insurance number" }, value: { text: referral.ni_number } }
-        )
-      end
-
-      if referral.trn.blank?
-        rows.push(
-          {
-            key: {
-              text: "Do you know their teacher reference number (TRN)?"
-            },
-            value: {
-              text: "No"
-            }
-          }
-        )
-      else
-        rows.push(
-          { key: { text: "Teacher reference number (TRN)?" }, value: { text: referral.trn } }
-        )
-      end
-
-      rows.push(
-        {
-          key: {
-            text: "Do they have qualified teacher status?"
-          },
-          value: {
-            text: referral.has_qts&.humanize
-          }
-        }
-      )
-      rows
+      summary_rows [
+                     first_name_row,
+                     last_name_row,
+                     previous_name_row,
+                     date_of_birth_row,
+                     email_address_row,
+                     phone_number_row,
+                     address_row,
+                     ni_number_row,
+                     trn_row,
+                     has_qts_row
+                   ].compact
     end
 
     def title
       "Personal details"
+    end
+
+    private
+
+    def first_name_row
+      { label: "First name", value: referral.first_name }
+    end
+
+    def last_name_row
+      { label: "Last name", value: referral.last_name }
+    end
+
+    def previous_name_row
+      if referral.name_has_changed == "no"
+        { label: "Do you know them by any other name?", value: "No" }
+      elsif referral.name_has_changed == "yes" && referral.previous_name.present?
+        { label: "Other name", value: referral.previous_name }
+      end
+    end
+
+    def date_of_birth_row
+      return unless referral.from_employer?
+
+      if referral.age_known
+        { label: "Date of birth", value: referral.date_of_birth&.to_fs(:long_ordinal_uk) }
+      else
+        { label: "Do you know their date of birth?", value: "No" }
+      end
+    end
+
+    def email_address_row
+      return unless referral.from_employer?
+
+      if referral.email_known
+        { label: "Email address", value: referral.email_address }
+      else
+        { label: "Do you know their email address?", value: "No" }
+      end
+    end
+
+    def phone_number_row
+      return unless referral.from_employer?
+
+      if referral.phone_known
+        { label: "Phone number", value: referral.phone_number }
+      else
+        { label: "Do you know their phone number?", value: "No" }
+      end
+    end
+
+    def address_row
+      return unless referral.from_employer?
+
+      if referral.address_known
+        { label: "Address", value: address(referral) }
+      else
+        { label: "Do you know their address?", value: "No" }
+      end
+    end
+
+    def ni_number_row
+      return unless referral.from_employer?
+
+      if referral.ni_number_known
+        { label: "National Insurance number", value: referral.ni_number }
+      else
+        { label: "Do you know their National Insurance number?", value: "No" }
+      end
+    end
+
+    def trn_row
+      return unless referral.from_employer?
+
+      if referral.trn.blank?
+        { label: "Do you know their teacher reference number (TRN)?", value: "No" }
+      else
+        { label: "Teacher reference number (TRN)?", value: referral.trn }
+      end
+    end
+
+    def has_qts_row
+      { label: "Do they have qualified teacher status?", value: referral.has_qts&.humanize }
     end
   end
 end
---
+++ spec/components/manage_interface/personal_details_component_spec.rb
@@ -0,0 +1,154 @@
+require "rails_helper"
+
+RSpec.describe ManageInterface::PersonalDetailsComponent, type: :component do
+  subject(:component) { described_class.new(referral:) }
+
+  let(:row_labels) { component.rows.map { |row| row.dig(:key, :text) } }
+  let(:row_values) { component.rows.map { |row| row.dig(:value, :text) }.map(&:strip) }
+  let(:row_values_sanitized) do
+    row_values.map { |value| ActionView::Base.full_sanitizer.sanitize(value) }
+  end
+
+  before { render_inline(component) }
+
+  context "with an employer referral" do
+    let(:referral) { create(:referral, :employer_complete) }
+
+    it "renders the correct labels" do
+      expect(row_labels).to eq(
+        [
+          "First name",
+          "Last name",
+          "Do you know them by any other name?",
+          "Do you know their date of birth?",
+          "Email address",
+          "Phone number",
+          "Address",
+          "National Insurance number",
+          "Teacher reference number (TRN)?",
+          "Do they have qualified teacher status?"
+        ]
+      )
+    end
+
+    it "renders the correct values" do
+      expect(row_values_sanitized).to eq(
+        [
+          "John",
+          "Smith",
+          "No",
+          "No",
+          "test@example.com",
+          "01234567890",
+          "1 Example StreetExample TownAB1 2CD",
+          "XH 25 91 97 C",
+          "1234567",
+          "Yes"
+        ]
+      )
+    end
+
+    context "when the previous name has changed" do
+      before { referral.update!(name_has_changed: "yes", previous_name: "Johnny Doe") }
+
+      it "renders the correct labels" do
+        expect(row_labels[2]).to eq("Other name")
+      end
+
+      it "renders the correct values" do
+        expect(row_values[2]).to eq("Johnny Doe")
+      end
+    end
+
+    context "when the date of birth is known" do
+      before { referral.update!(age_known: true, date_of_birth: Date.new(2000, 1, 1)) }
+
+      it "renders the correct labels" do
+        expect(row_labels[3]).to eq("Date of birth")
+      end
+
+      it "renders the correct values" do
+        expect(row_values[3]).to eq("1 January 2000")
+      end
+    end
+
+    context "when the email address is not known" do
+      before { referral.update!(email_known: false) }
+
+      it "renders the correct labels" do
+        expect(row_labels[4]).to eq("Do you know their email address?")
+      end
+
+      it "renders the correct values" do
+        expect(row_values[4]).to eq("No")
+      end
+    end
+
+    context "when the phone number is not known" do
+      before { referral.update!(phone_known: false) }
+
+      it "renders the correct labels" do
+        expect(row_labels[5]).to eq("Do you know their phone number?")
+      end
+
+      it "renders the correct values" do
+        expect(row_values[5]).to eq("No")
+      end
+    end
+
+    context "when the address is not known" do
+      before { referral.update!(address_known: false) }
+
+      it "renders the correct labels" do
+        expect(row_labels[6]).to eq("Do you know their address?")
+      end
+
+      it "renders the correct values" do
+        expect(row_values[6]).to eq("No")
+      end
+    end
+
+    context "when the NINO is not known" do
+      before { referral.update!(ni_number_known: false) }
+
+      it "renders the correct labels" do
+        expect(row_labels[7]).to eq("Do you know their National Insurance number?")
+      end
+
+      it "renders the correct values" do
+        expect(row_values[7]).to eq("No")
+      end
+    end
+
+    context "when the TRN is not known" do
+      before { referral.update!(trn: nil) }
+
+      it "renders the correct labels" do
+        expect(row_labels[8]).to eq("Do you know their teacher reference number (TRN)?")
+      end
+
+      it "renders the correct values" do
+        expect(row_values[8]).to eq("No")
+      end
+    end
+  end
+
+  context "with a public referral" do
+    let(:referral) { create(:referral, :public_complete) }
+
+    it "renders the correct labels" do
+      expect(row_labels).to eq(
+        [
+          "First name",
+          "Last name",
+          "Do you know them by any other name?",
+          "Do they have qualified teacher status?"
+        ]
+      )
+    end
+
+    it "renders the correct values" do
+      expect(row_values).to eq(%w[John Smith No Yes])
+    end
+  end
+end
