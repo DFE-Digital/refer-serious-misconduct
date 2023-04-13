d092169b18d2dd8a8f6cca1b7509a030aaba6c67
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-04-13 14:36:38 +0100

    Refactor ManageInterface::ReferrerDetailsComponent

---
+++ spec/components/manage_interface/referrer_details_component_spec.rb
@@ -0,0 +1,58 @@
+require "rails_helper"
+
+RSpec.describe ManageInterface::ReferrerDetailsComponent, type: :component do
+  subject(:component) { described_class.new(referral:) }
+
+  let(:row_labels) { component.rows.map { |row| row.dig(:key, :text) } }
+  let(:row_values) { component.rows.map { |row| row.dig(:value, :text) }.map(&:strip) }
+  let(:row_values_sanitized) do
+    row_values.map { |value| ActionView::Base.full_sanitizer.sanitize(value) }
+  end
+  let(:user) { create(:user, email: "test@example.com") }
+
+  before { render_inline(component) }
+
+  context "with an employer referral" do
+    let(:referral) { create(:referral, :employer_complete, user:) }
+
+    it "renders the correct labels" do
+      expect(row_labels).to eq(
+        [
+          "First name",
+          "Last name",
+          "Job title",
+          "Type",
+          "Email address",
+          "Phone number",
+          "Employer"
+        ]
+      )
+    end
+
+    it "renders the correct values" do
+      expect(row_values_sanitized).to eq(
+        [
+          "Jane",
+          "Smith",
+          "Headteacher",
+          "Employer",
+          "test@example.com",
+          "01234567890",
+          "Example School1 Example StreetExample CityAB1 2CD"
+        ]
+      )
+    end
+  end
+
+  context "with a public referral" do
+    let(:referral) { create(:referral, :public_complete, user:) }
+
+    it "renders the correct labels" do
+      expect(row_labels).to eq(["First name", "Last name", "Type", "Email address", "Phone number"])
+    end
+
+    it "renders the correct values" do
+      expect(row_values).to eq(%w[Jane Smith Public test@example.com 01234567890])
+    end
+  end
+end
