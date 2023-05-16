12ae2333cb5a25fefa9036d4df1d6f5c0a4961b1
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-05-16 18:28:47 +0100

    Fix deprecated methods in performance views
    The govuk-components methods weren't updated to the new v4.0 syntax
    when the gem was updated because these views are not covered by
    tests.

--- app/components/eligibility_performance_table_component.rb
+++ app/components/eligibility_performance_table_component.rb
@@ -7,45 +7,45 @@ class EligibilityPerformanceTableComponent < ViewComponent::Base
 
   def call
     govuk_table(classes: "app-performance-table") do |table|
-      table.head do |head|
-        head.row do |row|
-          row.cell(
+      table.with_head do |head|
+        head.with_row do |row|
+          row.with_cell(
             header: true,
             text: "Date",
             classes: "app-performance-table-column-divider app-performance-table-date-column"
           )
-          row.cell(header: true, text: "Complete")
-          row.cell(header: true, text: "Screened out")
-          row.cell(
+          row.with_cell(header: true, text: "Complete")
+          row.with_cell(header: true, text: "Screened out")
+          row.with_cell(
             header: true,
             text: "Did not finish",
             classes: "app-performance-table-column-divider"
           )
-          row.cell(header: true, text: "Total", classes: "govuk-!-padding-left-2")
+          row.with_cell(header: true, text: "Total", classes: "govuk-!-padding-left-2")
         end
       end
-      table.body do |body|
+      table.with_body do |body|
         grouped_request_counts.map do |period_label, counts|
-          body.row do |row|
-            row.cell(classes: "app-performance-table-column-divider") { period_label }
-            row.cell { number_with_percentage_cell(counts, :complete_count) }
-            row.cell { number_with_percentage_cell(counts, :screened_out_count) }
-            row.cell(classes: "app-performance-table-column-divider") do
+          body.with_row do |row|
+            row.with_cell(classes: "app-performance-table-column-divider") { period_label }
+            row.with_cell { number_with_percentage_cell(counts, :complete_count) }
+            row.with_cell { number_with_percentage_cell(counts, :screened_out_count) }
+            row.with_cell(classes: "app-performance-table-column-divider") do
               number_with_percentage_cell(counts, :incomplete_count)
             end
-            row.cell { "#{number_with_delimiter(counts[:total])} checks" }
+            row.with_cell { "#{number_with_delimiter(counts[:total])} checks" }
           end
         end
-        body.row(classes: "app-performance-table-total-row") do |row|
-          row.cell(header: true, classes: "app-performance-table-column-divider") do
+        body.with_row(classes: "app-performance-table-total-row") do |row|
+          row.with_cell(header: true, classes: "app-performance-table-column-divider") do
             "Total (#{since})"
           end
-          row.cell { number_with_percentage_cell(total_grouped_requests, :complete_count) }
-          row.cell { number_with_percentage_cell(total_grouped_requests, :screened_out_count) }
-          row.cell(classes: "app-performance-table-column-divider") do
+          row.with_cell { number_with_percentage_cell(total_grouped_requests, :complete_count) }
+          row.with_cell { number_with_percentage_cell(total_grouped_requests, :screened_out_count) }
+          row.with_cell(classes: "app-performance-table-column-divider") do
             number_with_percentage_cell(total_grouped_requests, :incomplete_count)
           end
-          row.cell { "#{number_with_delimiter(total_grouped_requests[:total])} checks" }
+          row.with_cell { "#{number_with_delimiter(total_grouped_requests[:total])} checks" }
         end
       end
     end
