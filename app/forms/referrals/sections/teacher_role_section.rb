1af7a3e0a2e59ba62bde54fd6e03b61ff841bfc2
Paul Odeon <mail@paulodeon.com>
2023-05-17 11:02:35 +0100

    Tidy section array builds

--- app/forms/referrals/sections/teacher_role_section.rb
+++ app/forms/referrals/sections/teacher_role_section.rb
@@ -2,39 +2,42 @@ module Referrals
   module Sections
     class TeacherRoleSection < Section
       def items
-        [
-          Referrals::TeacherRole::JobTitleForm.new(referral:),
-          Referrals::TeacherRole::DutiesForm.new(referral:)
-        ].tap do |items|
+        [].tap do |items|
+          items << Referrals::TeacherRole::JobTitleForm.new(referral:)
+          items << Referrals::TeacherRole::DutiesForm.new(referral:)
+
           if referral.from_employer?
-            items.append(Referrals::TeacherRole::SameOrganisationForm.new(referral:))
+            items << Referrals::TeacherRole::SameOrganisationForm.new(referral:)
           end
 
           unless referral.same_organisation?
-            items.append(Referrals::TeacherRole::OrganisationAddressKnownForm.new(referral:))
+            items << Referrals::TeacherRole::OrganisationAddressKnownForm.new(referral:)
+
             if referral.organisation_address_known?
-              items.append(Referrals::TeacherRole::OrganisationAddressForm.new(referral:))
+              items << Referrals::TeacherRole::OrganisationAddressForm.new(referral:)
             end
           end
 
           if referral.from_employer?
-            items.append(Referrals::TeacherRole::StartDateForm.new(referral:))
-            items.append(Referrals::TeacherRole::EmploymentStatusForm.new(referral:))
+            items << Referrals::TeacherRole::StartDateForm.new(referral:)
+            items << Referrals::TeacherRole::EmploymentStatusForm.new(referral:)
 
             if referral.left_role?
-              items.append(Referrals::TeacherRole::EndDateForm.new(referral:))
-              items.append(Referrals::TeacherRole::ReasonLeavingRoleForm.new(referral:))
-              items.append(Referrals::TeacherRole::WorkingSomewhereElseForm.new(referral:))
+              items << Referrals::TeacherRole::EndDateForm.new(referral:)
+              items << Referrals::TeacherRole::ReasonLeavingRoleForm.new(referral:)
+              items << Referrals::TeacherRole::WorkingSomewhereElseForm.new(referral:)
+
               if referral.working_somewhere_else?
-                items.append(Referrals::TeacherRole::WorkLocationKnownForm.new(referral:))
+                items << Referrals::TeacherRole::WorkLocationKnownForm.new(referral:)
+
                 if referral.work_location_known?
-                  items.append(Referrals::TeacherRole::WorkLocationForm.new(referral:))
+                  items << Referrals::TeacherRole::WorkLocationForm.new(referral:)
                 end
               end
             end
           end
 
-          items.append(Referrals::TeacherRole::CheckAnswersForm.new(referral:))
+          items << Referrals::TeacherRole::CheckAnswersForm.new(referral:)
         end
       end
 
