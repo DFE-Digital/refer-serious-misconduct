0d93366ccf34de3890028dd2052221117f49d40d
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-06-30 16:28:11 +0100

    Validate email address present if contact permission given

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -10,4 +10,5 @@ class Feedback < ApplicationRecord
   validates :satisfaction_rating, inclusion: { in: SATISFACTION_RATINGS }
   validates_presence_of :improvement_suggestion
   validates :contact_permission_given, inclusion: { in: [true, false] }
+  validates :email, presence: true, if: :contact_permission_given?
 end
