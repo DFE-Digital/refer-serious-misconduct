57b8abaef116194def7597c6ca222ba002c0d924
Graham Pengelly <graham@three-tier.com>
2023-07-13 14:25:08 +0100

    Add authentication to performance
    Move /perforamnce to /support/performance
    
    This will add authentication for staff users. We've never actually
    linked this page and it is more appropriate within the support section
    for now.

---
+++ app/controllers/support_interface/performance_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module SupportInterface
+  class PerformanceController < SupportInterfaceController
+    def index
+      stats = PerformanceStats.new
+
+      @total_requests_by_day = stats.total_requests_by_day
+      @request_counts_by_day = stats.request_counts_by_day
+      @total_requests_by_month = stats.total_requests_by_month
+      @request_counts_by_month = stats.request_counts_by_month
+      @daily_percentiles = stats.daily_percentiles
+      @total_percentiles = stats.total_percentiles
+    end
+  end
+end
