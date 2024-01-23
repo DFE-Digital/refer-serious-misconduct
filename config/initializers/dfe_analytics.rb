bcc82d0cd5783d62b4a63aeaaca89872bf22a008
Phil Lee <asmega@users.noreply.github.com>
2024-01-23 15:12:50 +0000

    enable DfE analytics entity checks

--- config/initializers/dfe_analytics.rb
+++ config/initializers/dfe_analytics.rb
@@ -1,6 +1,7 @@
 DfE::Analytics.configure do |config|
   config.queue = :analytics
   config.environment = HostingEnvironment.environment_name
+  config.entity_table_checks_enabled = true
 
   config.enable_analytics =
     proc do
