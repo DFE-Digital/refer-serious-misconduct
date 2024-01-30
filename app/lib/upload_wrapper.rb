7c7e8f3d582ec9b6c5b93eb41c2e737ea1f26e43
Richard Pattinson <richie.pattinson@googlemail.com>
2024-01-30 08:48:08 +0000

    Display count in upload error message

--- app/lib/upload_wrapper.rb
+++ app/lib/upload_wrapper.rb
@@ -1,14 +1,16 @@
 class UploadWrapper
   include ActiveSupport::NumberHelper
 
-  attr_reader :upload
+  attr_reader :upload, :count
 
-  def initialize(upload:)
+  def initialize(upload:, count:)
     @upload = upload
+    @count = count
   end
 
   def as_json(*args)
     r = super
-    r.merge(file_size: number_to_human_size(upload.size, strip_insignificant_zeros: false))
+    r.merge(file_size: number_to_human_size(upload.size, strip_insignificant_zeros: false),
+            files_with_errors_count: count)
   end
 end
--- spec/lib/upload_wrapper_spec.rb
+++ spec/lib/upload_wrapper_spec.rb
@@ -3,13 +3,18 @@ require "rails_helper"
 RSpec.describe UploadWrapper do
   describe "#as_json" do
     subject(:upload_wrapper) do
-      described_class.new(upload:)
+      described_class.new(upload:, count:)
     end
 
     let(:upload) { file_fixture("upload1.pdf") }
+    let(:count) { 4 }
 
     it "includes file size" do
       expect(upload_wrapper.as_json[:file_size]).to eql("4.98 KB")
+    end
+
+    it "includes the count" do
+      expect(upload_wrapper.as_json[:files_with_errors_count]).to be(4)
     end
   end
 end
