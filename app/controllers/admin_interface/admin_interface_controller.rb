48484c64404416fc5b2765c68fc3776d766be8f1
Phil Lee <asmega@users.noreply.github.com>
2024-03-01 09:23:54 +0000

    remove frozen_string_literal

--- app/controllers/admin_interface/admin_interface_controller.rb
+++ app/controllers/admin_interface/admin_interface_controller.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 module AdminInterface
   class AdminInterfaceController < ApplicationController
     include Pundit::Authorization
--- app/controllers/admin_interface/feedback_controller.rb
+++ app/controllers/admin_interface/feedback_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module AdminInterface
   class FeedbackController < AdminInterfaceController
     def index
