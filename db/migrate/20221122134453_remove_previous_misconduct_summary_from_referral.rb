e90c1e3fe724382054a84a90e5f5e6add94fda3f
Phil Lee <asmega@users.noreply.github.com>
2024-03-13 17:39:53 +0000

    tweak migrations for rails upgrade

--- db/migrate/20221122134453_remove_previous_misconduct_summary_from_referral.rb
+++ db/migrate/20221122134453_remove_previous_misconduct_summary_from_referral.rb
@@ -1,5 +1,7 @@
 class RemovePreviousMisconductSummaryFromReferral < ActiveRecord::Migration[7.0]
   def change
-    remove_column :referrals, :previous_misconduct_summary, :text, if_exists: true
+    if column_exists?(:referrals, :previous_misconduct_summary)
+      remove_column :referrals, :previous_misconduct_summary, :text
+    end
   end
 end
--- db/migrate/20230217122304_remove_referral_evidences_categories_other.rb
+++ db/migrate/20230217122304_remove_referral_evidences_categories_other.rb
@@ -1,5 +1,7 @@
 class RemoveReferralEvidencesCategoriesOther < ActiveRecord::Migration[7.0]
   def change
-    remove_column :referral_evidences, :categories_other, type: :string, if_exists: true
+    if column_exists?(:referral_evidences, :categories_other)
+      remove_column :referral_evidences, :categories_other, type: :string
+    end
   end
 end
