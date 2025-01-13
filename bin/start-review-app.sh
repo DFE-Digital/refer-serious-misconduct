c4d65bffdfa4a2d1daa1b6ccb9a467a372adeed8
RMcVelia <rick.mcvelia@gmail.com>
2025-01-13 16:16:46 +0000

    Add SNYK scan to build image

--- bin/start-review-app.sh
+++ bin/start-review-app.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-/usr/sbin/sshd
-
 bundle exec rails db:schema_load_or_migrate
-bundle exec rails runner "%i(eligibility_screener referral_form).each {|flag| FeatureFlags::FeatureFlag.activate(flag)}" 
+bundle exec rails runner "%i(eligibility_screener referral_form).each {|flag| FeatureFlags::FeatureFlag.activate(flag)}"
 bundle exec rails server -b 0.0.0.0
